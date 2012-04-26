0447772add9a3de0f141fb39db35476710edfc62
Russ Garrett <russ@garrett.co.uk>
2012-04-26 22:51:26 +0100

    Add bitfolk thanks and (unlinked) sponsors page

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+@app.route("/sponsors")
+def sponsors():
+    return render_template('sponsors.html')
+
 @app.route("/about/company")
 def company():
     return render_template('company.html')
