9cc4e7abb247439d1603227cd654e20d8c9ebe18
Russ Garrett <russ@garrett.co.uk>
2012-05-06 16:32:13 +0100

    Add password-setting using bcrypt

--- models/user.py
+++ models/user.py
@@ -1,6 +1,14 @@
 from main import db
+import bcrypt
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    password = db.Column(db.String)
+
+    def set_password(self, password):
+        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    def check_password(self, password):
+        return bcrypt.hashpw(password, self.password) == self.password
