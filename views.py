5ef3e05466dd1d7dc0b18d36b1c938a8cd673fdc
Russ Garrett <russ@garrett.co.uk>
2012-05-06 16:23:32 +0100

    Login form

--- views.py
+++ views.py
@@ -1,14 +1,22 @@
 from main import app
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from flaskext.login import login_user, login_required, logout_user
+from flaskext.wtf import Form, TextField, PasswordField, Required
 
 @app.route("/")
 def main():
     return render_template('main.html')
 
-@app.route("/login")
+class LoginForm(Form):
+    username = TextField('User Name', [Required()])
+    password = PasswordField('Password', [Required()])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return redirect('/')
+    return render_template("login.html", form=form)
 
 @app.route("/signup")
 def signup():
