b58b191c2a0de737737637e47a6e0d31bd824ebf
Russ Garrett <russ@garrett.co.uk>
2012-05-06 16:42:18 +0100

    Add email column

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ import bcrypt
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
     name = db.Column(db.String)
     password = db.Column(db.String)
 
