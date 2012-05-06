2b07c1877ebfcfdfcd6dffe589d48354b68f7fb0
Russ Garrett <russ@garrett.co.uk>
2012-05-06 17:06:46 +0100

    Gocardless lib and config

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ login_manager.setup_app(app)
 
 db = SQLAlchemy(app)
 
+import gocardless
+
+gocardless.environment = app.config['GOCARDLESS_ENVIRONMENT']
+gocardless.set_details(app_id=app.config['GOCARDLESS_APP_ID'],
+                        app_secret=app.config['GOCARDLESS_APP_SECRET'],
+                        access_token=app.config['GOCARDLESS_ACCESS_TOKEN'],
+                        merchant_id=app.config['GOCARDLESS_MERCHANT_ID'])
+
 from views import *
 from models import *
 db.create_all()
