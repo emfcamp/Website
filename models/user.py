dd268b20fe28fd363f1441ac9543d0cc74244b6d
Martin clarke <crypt9000@yahoo.com>
2012-05-30 01:24:25 +0200

    Added admin flag

--- models/user.py
+++ models/user.py
@@ -11,6 +11,7 @@ class User(db.Model, flaskext.login.UserMixin):
     email = db.Column(db.String, unique=True)
     name = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean, default=False, nullable=False)
     tickets = db.relationship('Ticket', lazy='dynamic', backref='user', cascade='all, delete, delete-orphan')
     payments = db.relationship('Payment', lazy='dynamic', backref='user', cascade='all')
 
