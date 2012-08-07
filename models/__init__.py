6803b883e27225f8d7fdd88540b4d9839ee966f9
Sam LR <sam.lindenrathen@gmail.com>
2012-08-07 22:29:51 +0100

    more pollution

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from user import *
 from payment import *
 from ticket import *
+from volunteers import *
--- views/volunteers.py
+++ views/volunteers.py
@@ -1,23 +1,24 @@
 from main import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flaskext.login import current_user
 from flaskext.wtf import Form, TextField, Email, Required, IntegerField
 from users import NextURLField
 
 class VolunteerLogin(Form):
     email = TextField('Email', [Email(), Required()])
-    phone_numer = IntegerField('Number', [Required()])
+    phone_number = IntegerField('Number', [Required()])
     next = NextURLField('Next')
     
 @app.route("/volunteer_login", methods=['GET', 'POST'])
 def volunteer_login():
-    if current_user.is_authenticated():
-        return redirect(request.args.get('next', url_for('tickets')))
+    if not current_user.is_authenticated():
+        return redirect(request.args.get('next', url_for('login')))
     form = VolunteerLogin(request.form, next=request.args.get('next'))
-    if request.method == 'POST' and form.validate():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user)
-            return redirect(form.next.data or url_for('tickets'))
-        else:
-            flash("Invalid login details!")
+    # if request.method == 'POST' and form.validate():
+        # user = User.query.filter_by(email=form.email.data).first()
+        # if user and user.check_password(form.password.data):
+        #     login_user(user)
+        #     return redirect(form.next.data or url_for('tickets'))
+        # else:
+        #     flash("Invalid login details!")
     return render_template('volunteer-login.html', form=form)
