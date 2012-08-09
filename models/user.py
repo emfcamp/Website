5d3f9b872dcf9dd7fcb18196566a297ab8877e20
Sam LR <sam.lindenrathen@gmail.com>
2012-08-09 02:02:13 +0100

    vague models for shifts and roles, added relationship to users

--- models/user.py
+++ models/user.py
@@ -14,6 +14,7 @@ class User(db.Model, flaskext.login.UserMixin):
     admin = db.Column(db.Boolean, default=False, nullable=False)
     tickets = db.relationship('Ticket', lazy='dynamic', backref='user', cascade='all, delete, delete-orphan')
     payments = db.relationship('Payment', lazy='dynamic', backref='user', cascade='all')
+    shifts = db.relationship('Shift', secondary='volunteer', backref=db.backref('users', lazy='dynamic'))
 
     def __init__(self, email, name):
         self.email = email
