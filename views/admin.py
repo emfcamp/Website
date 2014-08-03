c16705cb903224a017f7e5f1a774bffb551cc359
Mark Steward <marksteward@gmail.com>
2014-08-03 10:47:18 +0000

    No need to manage ticket types through web interface

--- views/admin.py
+++ views/admin.py
@@ -232,34 +232,6 @@ def make_admin():
     else:
         return(('', 404))
 
-class NewTicketTypeForm(Form):
-    name = TextField('Name', [Required()])
-    capacity = IntegerField('Capacity', [Required()])
-    limit = IntegerField('Limit', [Required()])
-    price_gbp = DecimalField('Price in GBP', [Required()])
-    price_eur = DecimalField('Price in EUR', [Required()])
-
-#@app.route("/admin/ticket-types", methods=['GET', 'POST'])
-@login_required
-def ticket_types():
-    if current_user.admin:
-        form = None
-        if request.method == 'POST':
-            form = NewTicketTypeForm()
-            if form.validate():
-                tt = TicketType(form.name.data, form.capacity.data, form.limit.data)
-                tt.prices = [TicketPrice('GBP', form.price_gbp), TicketPrice('EUR', form.price_eur)]
-                db.session.add(tt)
-                db.session.commit()
-                return redirect(url_for('ticket_types'))
-
-        types = TicketType.query.all()
-        if not form:
-            form = NewTicketTypeForm(formdata=None)
-        return render_template('admin/admin_ticket_types.html', types=types, form=form)
-    else:
-        return(('', 404))
-
 @app.route('/admin/payments')
 @admin_required
 def admin_payments():
