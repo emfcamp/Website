8fe8b766fe672413a526590ab4248acf406ba7bf
Mark Steward <marksteward@gmail.com>
2014-08-10 12:35:35 +0000

    Some more flake8 exceptions

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
-from user import *
-from payment import *
-from ticket import *
-from volunteers import *
-from cfp import *
+from user import *  # noqa
+from payment import *  # noqa
+from ticket import *  # noqa
+from volunteers import *  # noqa
+from cfp import *  # noqa
