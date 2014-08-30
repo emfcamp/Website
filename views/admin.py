9af4dc78275c06583b7a4168580146cd065eb7f2
Mark Steward <marksteward@gmail.com>
2014-08-30 11:15:08 +0000

    Missing import

--- views/admin.py
+++ views/admin.py
@@ -1,7 +1,7 @@
 from main import app, db, mail
 from models.user import User
 from models.payment import Payment, BankPayment, BankTransaction
-from models.ticket import Ticket
+from models.ticket import Ticket, TicketCheckin
 from models.cfp import Proposal
 from views import Form
 from views.payment.stripe import (
