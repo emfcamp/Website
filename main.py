e26ab11840b58c32ee8d9ec5f9ed0bf92e94e42e
Russ Garrett <russ@garrett.co.uk>
2015-12-09 15:04:41 +0000

    Install error handler in the right place...

--- main.py
+++ main.py
@@ -71,6 +71,14 @@ def create_app():
 
         stripe.api_key = app.config['STRIPE_SECRET_KEY']
 
+    @app.errorhandler(404)
+    def handle_404(e):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def handle_500(e):
+        return render_template('errors/500.html'), 500
+
     from apps.common import load_utility_functions
     load_utility_functions(app)
 
@@ -113,14 +121,6 @@ if __name__ == "__main__":
             if request.environ.get('HTTP_X_FORWARDED_PROTO') == 'https':
                 request.environ['wsgi.url_scheme'] = 'https'
                 _request_ctx_stack.top.url_adapter.url_scheme = 'https'
-
-    @app.errorhandler(404)
-    def handle_404(e):
-        return render_template('errors/404.html'), 404
-
-    @app.errorhandler(500)
-    def handle_500(e):
-        return render_template('errors/500.html'), 500
 
     if os.path.exists('.inside-vagrant'):
         # Make it easier to access from host machine
