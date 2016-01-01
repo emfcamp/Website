105e4019f455806398f32ea3cab00161b3267418
SamCook42 <gitsam@samlr.com>
2016-01-01 21:54:59 +0000

    Add feature flag function to templates
    Because feature flags can now be set in the db we can no longer simply
    check the config and instead need to call the function. This needs to be
    added to the template.

--- apps/common/__init__.py
+++ apps/common/__init__.py
@@ -59,7 +59,8 @@ def load_utility_functions(app_obj):
             SITE_STATE=SITE_STATE,
             CURRENCIES=CURRENCIES,
             CURRENCY_SYMBOLS=CURRENCY_SYMBOLS,
-            external_url=external_url
+            external_url=external_url,
+            is_feature_flag_set=is_feature_flag_set
         )
 
     @app_obj.context_processor
@@ -116,20 +117,26 @@ def process_basket():
     return basket, total
 
 
+# Decorator to control routes and functions
 def feature_flag(flag):
+    def call(f, *args, **kw):
+        if is_feature_flag_set(flag):
+            return f(*args, **kw)
+        return abort(404)
+    return decorator(call)
+
+
+# Simple function, used in templates
+def is_feature_flag_set(flag):
     """
     If the feature flag is present in the database use that, otherwise fall
     back to using the config file.
 
     If the flag isn't set 404 (or is set to False).
     """
-    def call(f, *args, **kw):
-        db_flag = FeatureFlag.get_flag(flag)
-        is_set_in_db = db_flag and (db_flag.enabled is True)
-        is_config_fallback = (db_flag is None) and\
-                             (app.config.get(flag, False) is True)
+    db_flag = FeatureFlag.get_flag(flag)
+    is_set_in_db = db_flag and (db_flag.enabled is True)
+    is_config_fallback = (db_flag is None) and\
+                         (app.config.get(flag, False) is True)
 
-        if is_set_in_db or is_config_fallback:
-            return f(*args, **kw)
-        return abort(404)
-    return decorator(call)
+    return is_set_in_db or is_config_fallback
