dbf978c32f4362056d565ea2148222cfb5f4edcf
Russ Garrett <russ@garrett.co.uk>
2016-03-06 13:38:21 +0000

    Ticket sales tier 3

--- utils.py
+++ utils.py
@@ -238,7 +238,7 @@ class CreateTickets(Command):
             # Leave order 0 & 1 free for discount tickets
             (0, 2, 'full', 'Full Camp Ticket', 193, 10, 100.00, 140.00, True, None),
             (1, 3, 'full', 'Full Camp Ticket', 350, 10, 110.00, 145.00, True, None),
-            # (2, 4, 'full', 'Full Camp Ticket', 300, 10, 110.00, 155.00, True, None),
+            (2, 4, 'full', 'Full Camp Ticket', 450, 10, 120.00, 158.00, True, None),
             # (3, 5, 'full', 'Full Camp Ticket', 400, 10, 120.00, 165.00, True, None),
             (3, 8, 'full', 'Full Camp Ticket (Supporter)', 56, 10, 130.00, 180.00, True,
                 "Support this non-profit event by paying a bit more. "
