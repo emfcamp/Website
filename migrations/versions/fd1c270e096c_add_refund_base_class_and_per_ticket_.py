603e13c195c875f1d84a0e0fe3f286937de65753
Mark Steward <marksteward@gmail.com>
2016-06-25 22:54:25 +0000

    Fix alembic defaults

--- migrations/versions/fd1c270e096c_add_refund_base_class_and_per_ticket_.py
+++ migrations/versions/fd1c270e096c_add_refund_base_class_and_per_ticket_.py
@@ -30,7 +30,7 @@ def upgrade():
     with op.batch_alter_table(u'proposal', schema=None) as batch_op:
         batch_op.alter_column('needs_help',
                existing_type=sa.BOOLEAN(),
-               nullable=False, default=False)
+               nullable=False, server_default=sa.false())
 
     with op.batch_alter_table(u'ticket', schema=None) as batch_op:
         batch_op.add_column(sa.Column('refund_id', sa.Integer(), nullable=True))
