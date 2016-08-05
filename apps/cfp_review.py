d91dbfff5cbcabf4696dfe77af743542022493b3
Jonty Wareing <jonty@jonty.co.uk>
2016-08-05 17:09:20 +0100

    Show everything in tweak view

--- apps/cfp_review.py
+++ apps/cfp_review.py
@@ -1060,8 +1060,7 @@ def potential_schedule_changes():
 @schedule_required
 def scheduler():
     proposals = Proposal.query.filter(Proposal.scheduled_duration.isnot(None)).\
-        filter(Proposal.state.in_(['finished', 'accepted'])).\
-        filter(Proposal.type.in_(['talk', 'workshop'])).all()
+        filter(Proposal.state.in_(['finished', 'accepted'])).all()
 
     schedule_data = []
     for proposal in proposals:
