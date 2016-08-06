587ac7a28a0378e552fd643fa9148ea720b18e2f
Mark Steward <marksteward@gmail.com>
2016-08-06 11:44:27 +0000

    Add /herald

--- apps/base.py
+++ apps/base.py
@@ -208,3 +208,9 @@ def design_elements():
 @base.route("/googlec108e6ab4f75019d.html")
 def google_verification_russ():
     return "google-site-verification: googlec108e6ab4f75019d.html"
+
+@base.route("/herald")
+def herald():
+    return redirect('https://wiki.emfcamp.org/wiki/Herald')
+
+
