a8df8099394ed4ddd71ba6f0030670f5b7d04724
SamCook42 <gitsam@samlr.com>
2017-09-29 02:49:58 +0100

    [WIP] Refactor Tickets
    This changes the model for tickets/products. The model is now:
    
    ProductGroup (capacity, expiry, parent ProductGroup)
         |
    PriceTier (a subtype of ProductGroup)
         |
    Price (value, currency)
         |
    ProductInstance (user, state, payment)
    
    By making ProductGroups recursive we gain a hierarchy of
    capacities/expiries hence no distinction between
    site-capacity/allocations/ticket types. Hopefully this will simplify
    things.

---
+++ models/product_group.py
@@ -0,0 +1,228 @@
+# coding=utf-8
+from decimal import Decimal
+from datetime import datetime, timedelta
+
+from main import db
+
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import column_property
+
+# state: [allowed next state, ] pairs
+PRODUCT_INSTANCE_STATES = { 'reserved': ['payment-pending', 'expired'],
+                            'payment-pending': ['expired', 'paid'],
+                            'expired': [],
+                            'paid': ['checked-in', 'refunded'],
+                            'refunded': [],
+                            # allow undoing of check-in
+                            'checked-in': ['paid'],
+                            }
+allowed_states = list(PRODUCT_INSTANCE_STATES.keys())
+
+# In hours
+PRODUCT_INSTANCE_EXPIRY_TIME = 2
+
+
+class ProductInstanceStateException(Exception):
+    pass
+
+
+class ProductGroupException(Exception):
+    pass
+
+class ProductGroup(db.Model):
+    __tablename__ = "product_group"
+    __mapper_args__ = {
+        'polymorphic_on': 'type',
+        'polymorphic_identity': 'product_group',
+    }
+
+    # This is a self referential table. Types have a parent type which
+    # they also validate against.
+    id = db.Column(db.Integer, primary_key=True)
+    parent_id = db.Column(db.Integer, db.ForeignKey("product_group.id"))
+    type = db.Column(db.String, nullable=False)
+
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String)
+
+    expiry = db.Column(db.DateTime)
+    # A max capacity of -1 implies no max (use parent"s if set)
+    capacity_max = db.Column(db.Integer, default=-1)
+    capacity_used = db.Column(db.Integer, default=0)
+    capacity_remaining = column_property(capacity_max - capacity_used)
+
+    parent = db.relationship("ProductGroup", remote_side=[id], backref="children", cascade="all")
+
+    db.CheckConstraint("capacity_used <= capacity_max", "within_capacity")
+
+    def __init__(self, name, **kwargs):
+        super().__init__(name=name, **kwargs)
+
+    def __repr__(self):
+        return "<%s: %s>" % (self.__class__.__name__, self.name)
+
+    def get_counts_by_state(self, states_to_get=allowed_states, res={}):
+        if len(res) == 0:
+            res = {s: 0 for s in states_to_get}
+
+        for child in self.children:
+            res = child.get_counts_by_state(states_to_get, res)
+        return res
+
+    def get_sold(self):
+        return self.get_counts_by_state(['paid', 'checked-in'])
+
+    def has_capacity(self, count=1):
+        """
+        Determine whether this ProductGroup, and all its ancestors, have
+        available capacity.
+
+        The count parameter (default: 1) determines whether there is capacity
+        for count instances.
+        """
+        if count < 1:
+            raise ValueError("Count cannot be less than 1.")
+
+        if self.parent and not self.parent.has_capacity():
+            return False
+
+        if self.capacity_max >= 0:
+            return (self.capacity_used + count) <= self.capacity_max
+        return True
+
+    def has_expired(self):
+        """
+        Determine whether this ProductGroup, and any of its ancestors, have
+        expired.
+        """
+        if self.parent and self.parent.has_expired():
+            return True
+
+        if self.expiry:
+            return datetime.utcnow() > self.expiry
+        return False
+
+    def issue_instances(self, count=1):
+        """
+        If possible (i.e. the ProductGroup has not expired and has capacity)
+        reduce the available capacity by count.
+        """
+        if not self.has_capacity(count):
+            raise ProductGroupException("ProductGroup is out of capacity.")
+
+        if self.has_expired():
+            raise ProductGroupException("ProductGroup has expired.")
+
+        if self.parent:
+            self.parent.issue_instances(count)
+        self.capacity_used += count
+
+    def return_instances(self, count=1):
+        """
+        Reintroduce previously used capacity
+        """
+        if count < 1:
+            raise ValueError("Count cannot be less than 1.")
+        self.parent.return_instances(count)
+        self.capacity_used -= count
+
+    # This is mostly used in testing...
+    @classmethod
+    def get_by_name(cls, name):
+        return ProductGroup.query.filter_by(name=name).first()
+
+
+class PriceTier(ProductGroup):
+    __mapper_args__ = {'polymorphic_identity': 'price_tier'}
+
+    def get_counts_by_state(self, states_to_get=allowed_states, res={}):
+        if res == {}:
+            res = {s: 0 for s in states_to_get}
+
+        for purchase in self.purchases:
+            state = purchase.state
+            if state in states_to_get:
+                res[state] += 1
+
+        return res
+
+
+class Price(db.Model):
+    __tablename__ = "product_price"
+    # Only allow 1 price per currency per price tier
+    __table_args__ = (
+        UniqueConstraint('price_tier_id', 'currency', name='_product_currency_uniq'),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    price_tier_id = db.Column(db.Integer, db.ForeignKey("product_group.id"), nullable=False)
+    currency = db.Column(db.String, nullable=False)
+    price_int = db.Column(db.Integer, nullable=False)
+    price_tier = db.relationship(PriceTier, backref=db.backref("prices", cascade="all"))
+
+    @property
+    def value(self):
+        return Decimal(self.price_int) / 100
+
+    @value.setter
+    def value(self, val):
+        self.price_int = int(val * 100)
+
+
+class ProductInstance(db.Model):
+    __versioned__ = {}
+    __tablename__ = "product_instance"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    price_tier_id = db.Column(db.Integer, db.ForeignKey('product_group.id'), nullable=False)
+    price_id = db.Column(db.Integer, db.ForeignKey('product_price.id'))
+
+    # History
+    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    modified = db.Column(db.DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
+
+    # State tracking info
+    state = db.Column(db.String, default='reserved', nullable=False)
+    # Until a ticket is paid for, we track the payment's expiry
+    expires = db.Column(db.DateTime, nullable=False)
+
+    # Financial tracking
+    payment_id = db.Column(db.Integer, db.ForeignKey('payment.id'))
+    refund_id = db.Column(db.Integer, db.ForeignKey('refund.id'))
+
+    # Relationships
+    price = db.relationship(Price, backref='purchases')
+    price_tier = db.relationship(PriceTier, backref='purchases')
+    payment = db.relationship('Payment', backref='purchase')
+    refund = db.relationship('Refund', backref='purchase')
+
+    def __init__(self, **kwargs):
+        expires = datetime.utcnow() + timedelta(hours=PRODUCT_INSTANCE_EXPIRY_TIME)
+        super().__init__(expires=expires, **kwargs)
+
+    def set_state(self, new_state):
+        new_state = new_state.lower()
+
+        if new_state not in PRODUCT_INSTANCE_STATES:
+            raise ProductInstanceStateException('"%s" is not a valid state.')
+
+        if new_state not in PRODUCT_INSTANCE_STATES[self.state]:
+            raise ProductInstanceStateException('"%s->%s" is not a valid transition' % (self.state, new_state))
+
+        self.state = new_state
+
+    @classmethod
+    def create_instances(self, user, tier, currency, count=1):
+        try:
+            price = [p for p in tier.prices if p.currency == currency.lower()][0]
+        except IndexError:
+            msg = 'Could not find currency, %s, for %s' % (currency, tier)
+            raise ProductGroupException(msg)
+
+        tier.issue_instances(count)
+
+        return [ProductInstance(price_tier_id=tier.id,
+                                user_id=user.id,
+                                price_id=price.id,
+                ) for c in range(count)]
---
+++ tests/test_product_group.py
@@ -0,0 +1,287 @@
+# coding=utf-8
+import unittest
+
+from decimal import Decimal
+from datetime import datetime
+from unittest.mock import patch, Mock
+
+from .core import get_app
+from models.user import User
+from models.product_group import (
+    PRODUCT_INSTANCE_STATES, ProductGroupException, ProductInstanceStateException,
+    ProductGroup, PriceTier, Price, ProductInstance
+)
+
+class SingleProductGroupTest(unittest.TestCase):
+    item_name = 'killer_tent'
+
+    def get_item(self):
+        return ProductGroup.get_by_name(self.item_name)
+
+    def setUp(self):
+        self.client, self.app, self.db = get_app()
+        self.app.testing = True
+
+        with self.app.app_context():
+            item = ProductGroup(self.item_name, capacity_max=1, expiry=datetime(2012, 8, 31))
+            self.db.session.add(item)
+
+            self.db.session.commit()
+
+    def tearDown(self):
+        with self.app.app_context():
+            self.db.session.delete(self.get_item())
+            self.db.session.commit()
+
+    def test_has_capacity(self):
+        with self.app.app_context():
+            # With no parent this is a trivial test
+            item = self.get_item()
+            self.assertTrue(item.has_capacity())
+
+            with self.assertRaises(ValueError):
+                item.has_capacity(-1)
+
+
+    def test_has_expired(self):
+        with self.app.app_context():
+            item = self.get_item()
+
+            with patch('models.product_group.datetime') as mock_good_datetime:
+                mock_good_datetime.utcnow = Mock(return_value=datetime(2012, 8, 2))
+                self.assertFalse(item.has_expired())
+
+            with patch('models.product_group.datetime') as mock_expired_datetime:
+                mock_expired_datetime.utcnow = Mock(return_value=datetime(2012, 9, 2))
+                self.assertTrue(item.has_expired())
+
+    def test_issue_instance(self):
+        with self.app.app_context():
+            item = self.get_item()
+
+            # Will raise an error if we try to issue once expired
+            with patch('models.product_group.datetime') as mock_expired_datetime:
+                mock_expired_datetime.utcnow = Mock(return_value=datetime(2012, 9, 2))
+
+                with self.assertRaises(ProductGroupException):
+                    item.issue_instances()
+
+
+            # Now test with a good value for now()
+            with patch('models.product_group.datetime') as mock_good_datetime:
+                mock_good_datetime.utcnow = Mock(return_value=datetime(2012, 8, 2))
+
+                item.issue_instances()
+                self.db.session.commit()
+
+                self.assertFalse(item.has_capacity())
+                with self.assertRaises(ProductGroupException):
+                    item.issue_instances()
+
+    def test_capacity_remaining(self):
+        with self.app.app_context():
+            item = self.get_item()
+            self.assertEqual(item.capacity_max, item.capacity_remaining)
+
+            item.capacity_used = item.capacity_max
+
+            self.db.session.commit()
+            self.assertEqual(0, item.capacity_remaining)
+
+
+class MultipleProductGroupTest(unittest.TestCase):
+    parent_name = 'parent'
+    group1_name = 'child1'
+    group2_name = 'child2'
+
+    def get_item(self, name):
+        return ProductGroup.get_by_name(name)
+
+    def setUp(self):
+        self.client, self.app, self.db = get_app()
+        self.app.testing = True
+
+        with self.app.app_context(), self.db.session.no_autoflush:
+
+            parent = ProductGroup(self.parent_name, capacity_max=3)
+            item1 = ProductGroup(self.group1_name, parent=parent)
+            self.db.session.add(item1)
+
+            item2 = ProductGroup(self.group2_name, parent=parent)
+            self.db.session.add(item2)
+
+            self.db.session.commit()
+
+    def tearDown(self):
+        with self.app.app_context():
+            # deletes should cascade so just delete the parent
+            item = self.get_item(name=self.parent_name)
+            self.db.session.delete(item)
+            self.db.session.commit()
+
+    # We want to mostly check that capacities are inherited & shared
+    def test_has_capacity_cascades(self):
+        with self.app.app_context():
+            item1 = self.get_item(name=self.group1_name)
+            item1.issue_instances(3)
+            self.db.session.commit()
+
+            parent = self.get_item(name=self.parent_name)
+            item2 = self.get_item(name=self.group2_name)
+            self.assertFalse(parent.has_capacity())
+            self.assertFalse(item1.has_capacity())
+            self.assertFalse(item2.has_capacity())
+
+
+class ProductInstanceTest(unittest.TestCase):
+    pg_name = 'pg'
+    tier_name = 'tier'
+    user_email = 'a@b.c'
+
+    def get_instance(self, session, tier=None):
+        user = User.get_by_email(self.user_email)
+        if tier is None:
+            tier = PriceTier.get_by_name(self.tier_name)
+
+        instance = ProductInstance.create_instances(user, tier, 'gbp')[0]
+
+        session.add(instance)
+        session.commit()
+
+        return instance
+
+    def setUp(self):
+        self.client, self.app, self.db = get_app()
+        self.app.testing = True
+
+        with self.app.app_context():
+
+            user = User(self.user_email, 'test_user')
+            self.db.session.add(user)
+
+            parent = ProductGroup(self.pg_name, capacity_max=3)
+            tier = PriceTier(self.tier_name, parent=parent)
+            price = Price(price_tier=tier, currency="gbp", price_int=666)
+            # These have `cascade=all` so just add the bottom of the hierarchy
+            self.db.session.add(price)
+
+            self.db.session.commit()
+
+    def tearDown(self):
+        with self.app.app_context():
+            self.db.session.delete(User.get_by_email(self.user_email))
+            self.db.session.delete(ProductGroup.get_by_name(self.pg_name))
+            self.db.session.commit()
+
+    def test_create_instances(self):
+        with self.app.app_context():
+            user = User.get_by_email(self.user_email)
+            tier = PriceTier.get_by_name(self.tier_name)
+            self.assertEqual(0, tier.capacity_used)
+
+            instance = self.get_instance(self.db.session)
+
+            self.assertEqual(1, tier.capacity_used)
+            self.assertEqual(1, ProductGroup.get_by_name(self.pg_name).capacity_used)
+
+            # NB: Decimal('6.66') != Decimal(6.66) == Decimal(float(6.66)) ~= 6.6600000000000001
+            self.assertEqual(instance.price.value, Decimal('6.66'))
+
+            # Test bad currencies error
+            with self.assertRaises(ProductGroupException):
+                ProductInstance.create_instances(user, tier, 'wtf')
+
+            # Test issuing multiple instances works
+            more_instances = ProductInstance.create_instances(user, tier, 'gbp', 2)
+            self.assertEqual(2, len(more_instances))
+            self.assertEqual(3, ProductGroup.get_by_name(self.pg_name).capacity_used)
+
+            # Test issuing beyond capacity errors
+            with self.assertRaises(ProductGroupException):
+                ProductInstance.create_instances(user, tier, 'gbp')
+
+    def test_product_instance_state_machine(self):
+        states_dict = PRODUCT_INSTANCE_STATES
+
+        # 'reserved' is the start state, all other states must
+        # exist as the next_state of some state.
+        # e.g. "payment-pending" and "paid" are next states for
+        # "reserved" and "payment-pending" respectively.
+        self.assertIn('reserved', states_dict)
+        seen_in_next_states = list(states_dict.keys())
+        seen_in_next_states.remove('reserved')
+
+        for state in states_dict:
+            next_states = states_dict[state]
+
+            for allowed_state in next_states:
+                self.assertIn(allowed_state, states_dict)
+                if allowed_state in seen_in_next_states:
+                    seen_in_next_states.remove(allowed_state)
+
+        self.assertEqual(0, len(seen_in_next_states), 'Found unreachable states: %s' % seen_in_next_states)
+
+    def test_set_state(self):
+        with self.app.app_context():
+            instance = self.get_instance(self.db.session)
+
+            with self.assertRaises(ProductInstanceStateException):
+                instance.set_state('disallowed-state')
+
+            with self.assertRaises(ProductInstanceStateException):
+                instance.set_state('paid')
+
+            instance.set_state('payment-pending')
+
+            self.assertEqual('payment-pending', instance.state)
+
+    def test_product_group_get_counts_by_state(self):
+        with self.app.app_context():
+            # Test it works at the PriceTier level
+            tier1 = PriceTier.get_by_name(self.tier_name)
+            instance1 = self.get_instance(self.db.session)
+
+            states_count = tier1.get_counts_by_state()
+            expect = {s: 0 for s in PRODUCT_INSTANCE_STATES.keys()}
+            expect['reserved'] = 1
+
+            self.assertEqual(expect, states_count)
+
+            # Now test we see the same in a ProductGroup
+            parent = ProductGroup.get_by_name(self.pg_name)
+
+            parent_states = parent.get_counts_by_state()
+
+            self.assertEqual(expect, parent_states)
+
+            # Test that other states show up
+            instance1.set_state('payment-pending')
+            self.db.session.commit()
+
+            parent_states = parent.get_counts_by_state()
+            expect['reserved'] = 0
+            expect['payment-pending'] = 1
+
+            self.assertEqual(expect, parent_states)
+
+            # Add another instance in another tier
+            tier2 = PriceTier('2', parent=parent)
+            price = Price(price_tier=tier2, currency="gbp", price_int=666)
+            self.db.session.add(price)
+            # rely on the commit in 'get_instance'
+            self.get_instance(self.db.session, tier2)
+
+            parent_states = parent.get_counts_by_state()
+            expect['reserved'] = 1
+
+            self.assertEqual(expect, parent_states)
+
+    def test_get_sold(self):
+        with self.app.app_context():
+            # Test it works at the PriceTier level
+            tier = PriceTier.get_by_name(self.tier_name)
+            instance = self.get_instance(self.db.session)
+
+            instance.state = 'paid'
+
+            self.assertEqual({'paid': 1, 'checked-in': 0}, tier.get_sold())
