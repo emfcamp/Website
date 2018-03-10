23f381c321b911dc8632f793b2e6fab79107101c
Russ Garrett <russ@garrett.co.uk>
2018-03-10 11:25:21 +0000

    Allow tests to test prometheus metric collection

--- tests/core.py
+++ tests/core.py
@@ -2,6 +2,7 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 import os
 import os.path
+import shutil
 from main import create_app, db
 from utils import CreateBankAccounts, CreateTickets
 
@@ -10,6 +11,14 @@ def get_app():
     if 'SETTINGS_FILE' not in os.environ:
         root = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
         os.environ['SETTINGS_FILE'] = os.path.join(root, 'config', 'test.cfg')
+
+    prometheus_dir = '/tmp/emf_test_prometheus'
+    os.environ['prometheus_multiproc_dir'] = prometheus_dir
+
+    if os.path.exists(prometheus_dir):
+        shutil.rmtree(prometheus_dir)
+    if not os.path.exists(prometheus_dir):
+        os.mkdir(prometheus_dir)
 
     app = create_app()
 
