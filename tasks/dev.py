e84191c9c56fe03dd634d8b0b75c439a5d309dd0
Russ Garrett <russ@garrett.co.uk>
2018-06-24 20:52:38 +0100

    Fake CfP votes

--- tasks/dev.py
+++ tasks/dev.py
@@ -1,14 +1,29 @@
-
 import random
 from faker import Faker
 from flask_script import Command
 
 from main import db
 from models.cfp import (TalkProposal, PerformanceProposal, WorkshopProposal,
-                        YouthWorkshopProposal, InstallationProposal, LENGTH_OPTIONS)
+                        YouthWorkshopProposal, InstallationProposal, CFPVote,
+                        LENGTH_OPTIONS)
 from models.user import User
 
-def fake_proposal(fake):
+def random_state(states):
+    cumulative = []
+    p = 0
+    for state, prob in states.items():
+        cumulative.append((state, p + prob))
+        p += prob
+    assert p == 1
+
+    r = random.random()
+    for state, prob in cumulative:
+        if r <= prob:
+            return state
+    assert False
+
+
+def fake_proposal(fake, reviewers):
     cfp = random.choice([TalkProposal, PerformanceProposal, WorkshopProposal,
                          YouthWorkshopProposal, InstallationProposal])()
     cfp.title = fake.sentence(nb_words=6, variable_nb_words=True)
@@ -23,14 +38,29 @@ def fake_proposal(fake):
 
     states = {'accepted': 0.1,
               'rejected': 0.05,
-              'anonymised': 0.1,
-              'checked': 0.1,
-              'reviewed': 0.1}
+              'anonymised': 0.2,
+              'checked': 0.2,
+              'anon-blocked': 0.05,
+              'new': 0.2,
+              'reviewed': 0.2}
 
-    for state, prob in states.items():
-        if random.random() < prob:
-            cfp.state = state
-            break
+    cfp.state = random_state(states)
+
+    vote_states = {
+        'voted': 0.65,
+        'recused': 0.1,
+        'blocked': 0.05,
+        'resolved': 0.1,
+        'stale': 0.1
+    }
+
+    if cfp.state in ('anonymised', 'reviewed', 'accepted') and \
+            type(cfp) in (TalkProposal, WorkshopProposal):
+        for reviewer in random.sample(reviewers, random.randint(0, len(reviewers))):
+            vote = CFPVote(reviewer, cfp)
+            vote.state = random_state(vote_states)
+            if vote.state in ('voted', 'stale', 'resolved'):
+                vote.vote = random.randint(0, 3)
 
     if type(cfp) in (WorkshopProposal, YouthWorkshopProposal):
         cfp.attendees = int(round(random.uniform(5, 50)))
@@ -55,10 +85,15 @@ class MakeFakeData(Command):
             user_anonymiser.grant_permission('cfp_anonymiser')
             db.session.add(user_anonymiser)
 
-        if not User.query.filter_by(email='reviewer@test.invalid').first():
-            user_reviewer = User('reviewer@test.invalid', 'Test Reviewer')
-            user_reviewer.grant_permission('cfp_reviewer')
-            db.session.add(user_reviewer)
+        reviewers = []
+        for i in range(10):
+            email = 'reviewer{}@test.invalid'.format(i)
+            user = User.query.filter_by(email=email).first()
+            if not user:
+                user = User(email, 'Reviewer {}'.format(i))
+                user.grant_permission('cfp_reviewer')
+                db.session.add(user)
+            reviewers.append(user)
 
         if not User.query.filter_by(email='arrivals@test.invalid').first():
             user_arrivals = User('arrivals@test.invalid', 'Test Arrivals')
@@ -73,7 +108,7 @@ class MakeFakeData(Command):
             user = User(email, fake.name())
 
             for i in range(0, int(round(random.uniform(0, 2)))):
-                cfp = fake_proposal(fake)
+                cfp = fake_proposal(fake, reviewers)
                 cfp.user = user
 
             db.session.add(user)
