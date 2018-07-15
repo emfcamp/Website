5b58ae28d6025273f55e20a20ccb99792a320117
SamCook42 <gitsam@samlr.com>
2018-07-15 22:01:06 +0100

    Basic volunteer schedule view
    Still very WIP

--- models/volunteer/shift.py
+++ models/volunteer/shift.py
@@ -1,6 +1,8 @@
 # coding=utf-8
-from main import db
+from sqlalchemy.orm import validates
+from pendulum import period
 
+from main import db
 
 class Shift(db.Model):
     __tablename__ = 'volunteer_shift'
@@ -18,8 +20,43 @@ class Shift(db.Model):
     role = db.relationship('Role', backref='shifts')
     venue = db.relationship('VolunteerVenue', backref='shifts')
 
+    @validates('start', 'end')
+    def validate_shift_times(self, key, datetime):
+        assert (datetime.minute % 15 == 0), '%s datetimes must be quarter-hour aligned' % key
+        return datetime
+
     def __repr__(self):
         return '<Shift {0}/{1}@{2}>'.format(self.role.name, self.venue.name, self.start)
+
+    def duration_in_minutes(self):
+        return (self.start - self.end).total_seconds() // 60
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.order_by(Shift.start, Shift.venue_id).all()
+
+    @classmethod
+    def generate_for(cls, role, venue, first, final, min, max, base_duration=180, changeover=15):
+        """
+        Will generate shifts between start and end times. The last shift will
+        start AT the end time (minus changeover time).
+        base_duration is the "base" length of a shift (not including changeover)
+        in minutes.
+        changeover is the changeover time in minutes.
+        This will mean that during changeover there will be two shifts created.
+        """
+        def start(t):
+            return t.subtract(minutes=changeover)
+
+        def end(t):
+            return t.add(minutes=base_duration)
+
+        initial_start_times = list(period(first.naive(), final.naive()).range('minutes', base_duration))
+
+
+        return [Shift(role=role, venue=venue, min_needed=min, max_needed=max,
+                      start=start(t), end=end(t))
+                for t in initial_start_times]
 
 
 class ShiftEntry(db.Model):
