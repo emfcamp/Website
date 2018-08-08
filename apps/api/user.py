58d814211906a319858983fa950a409b4f0f06fb
Russ Garrett <russ@garrett.co.uk>
2018-08-08 11:57:18 +0100

    More API tweaks

--- apps/api/user.py
+++ apps/api/user.py
@@ -1,13 +1,12 @@
-from flask import abort
 from flask_login import current_user
-from flask_restful import Resource
-from models.user import User
+from flask_restful import Resource, abort
+# from models.user import User
 from . import api
 
 
 def user_info(user):
     return {
-        "url": api.url_for(UserInfo, user_id=user.id),
+        # "url": api.url_for(UserInfo, user_id=user.id),
         "email": user.email,
         "name": user.name,
         "permissions": [p.name for p in user.permissions],
@@ -16,15 +15,17 @@ def user_info(user):
 
 class CurrentUserInfo(Resource):
     def get(self):
-        if not current_user:
-            abort(404)
+        if not current_user.is_authenticated:
+            abort(401)
         return user_info(current_user)
 
 
-class UserInfo(Resource):
-    def get(self, user_id):
-        return user_info(User.get(user_id))
+# Need to think about permissions here and I don't need this anyway at the moment
+# --Russ
+# class UserInfo(Resource):
+#    def get(self, user_id):
+#        return user_info(User.get(user_id))
 
 
 api.add_resource(CurrentUserInfo, "/user/current")
-api.add_resource(UserInfo, "/user/<int:user_id>")
+# api.add_resource(UserInfo, "/user/<int:user_id>")
