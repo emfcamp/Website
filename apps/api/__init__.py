09bdc036b81df44d835110e8306d809dc653b59a
Russ Garrett <russ@garrett.co.uk>
2018-08-08 20:50:39 +0100

    More API

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
+from main import csrf
 
 api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
+api = Api(api_bp, decorators=[csrf.exempt])
 
 from . import user  # noqa
 from . import map  # noqa
