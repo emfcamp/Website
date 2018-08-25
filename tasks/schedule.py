cf497f875f518c3d92ade44da83d80337c8e7ee3
Jonty Wareing <jonty@jonty.co.uk>
2018-08-26 00:42:03 +0100

    We don't autoschedule performances

--- tasks/schedule.py
+++ tasks/schedule.py
@@ -58,7 +58,7 @@ class RunScheduler(Command):
     def get_scheduler_data(self):
         proposals = Proposal.query.filter(Proposal.scheduled_duration.isnot(None)).\
             filter(Proposal.state.in_(['finished', 'accepted'])).\
-            filter(Proposal.type.in_(['talk', 'workshop', 'youthworkshop', 'performance'])).\
+            filter(Proposal.type.in_(['talk', 'workshop', 'youthworkshop'])).\
             order_by(Proposal.favourite_count.desc()).all()
 
         proposals_by_type = defaultdict(list)
