ca3d74f1254022a4341e5e9dd74725025756cc84
SamCook42 <gitsam@samlr.com>
2018-08-30 20:14:37 +0100

    shift filters

--- apps/volunteer/admin/shift.py
+++ apps/volunteer/admin/shift.py
@@ -3,4 +3,7 @@ from ..flask_admin_base import VolunteerModelView
 from main import volunteer_admin, db
 from models.volunteer.shift import Shift
 
-volunteer_admin.add_view(VolunteerModelView(Shift, db.session, category="shifts"))
+class ShiftModelView(VolunteerModelView):
+    column_filters = ['role', 'venue', 'start', 'end']
+
+volunteer_admin.add_view(ShiftModelView(Shift, db.session, category="shifts"))
