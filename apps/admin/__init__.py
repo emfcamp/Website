c986a4cf7a53bfcd71fb2548e0fce00fb7577b59
SamCook42 <gitsam@samlr.com>
2018-08-31 10:30:00 +0100

    Cope with None

--- apps/admin/__init__.py
+++ apps/admin/__init__.py
@@ -206,8 +206,8 @@ class ScheduleForm(Form):
         self.type.data = feed.type
         self.phone.data = feed.contact_phone
         self.email.data = feed.contact_email
-        self.lat.data = feed.latlon[0]
-        self.lon.data = feed.latlon[1]
+        self.lat.data = feed.latlon[0] if feed.latlon else 0.0
+        self.lon.data = feed.latlon[1] if feed.latlon else 0.0
         self.priority.data = feed.priority
 
 @admin.route('/schedule-feeds/<int:feed_id>', methods=['GET', 'POST'])
