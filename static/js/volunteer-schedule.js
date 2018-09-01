990035fd6d0bad70b83132a468da66f295836e85
SamCook42 <gitsam@samlr.com>
2018-09-01 18:01:49 +0100

    Use correct location

--- static/js/volunteer-schedule.js
+++ static/js/volunteer-schedule.js
@@ -88,7 +88,7 @@ function init_volunteer_schedule(data, all_roles, active_day, is_admin) {
 
             $('#modal-start-time').html(shift.start_time);
             $('#modal-end-time').html(shift.end_time);
-            $('#modal-location').html(shift.location);
+            $('#modal-location').html(shift.venue.name);
 
             var role_id = shift.role.name.toLowerCase().replace(/[^\w]+/g, '-');
             $('#modal-description').html($(`#role-description-${role_id}`).clone().show());
