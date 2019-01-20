5916620efc6649df5d04031a0489ad44f29a0684
Jon Wood <jon@blankpad.net>
2019-01-20 23:23:59 +0000

    Make Travis use Docker as well

--- prepare_db.sh
+++ prepare_db.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-docker-compose run postgres psql postgres://postgres@postgres -c 'CREATE DATABASE emf_site'
-docker-compose run postgres psql postgres://postgres@postgres/emf_site -c 'CREATE EXTENSION postgis'
-docker-compose run postgres psql postgres://postgres@postgres -c 'CREATE DATABASE emf_site_test'
-docker-compose run postgres psql postgres://postgres@postgres/emf_site_test -c 'CREATE EXTENSION postgis'
+PSQL="docker-compose run postgres psql postgres://postgres@postgres"
+
+until $PSQL -c '\q'; do
+  >&2 echo "Waiting for Postgres to be available"
+  sleep 1
+done
+
+$PSQL -c 'CREATE DATABASE emf_site'
+$PSQL/emf_site -c 'CREATE EXTENSION postgis'
+$PSQL -c 'CREATE DATABASE emf_site_test'
+$PSQL/emf_site_test -c 'CREATE EXTENSION postgis'
