71dbd53e9cff2c3f3d0fa88c85de617c6db7bea8
Russ Garrett <russ@garrett.co.uk>
2019-11-16 13:26:20 +0000

    Test reachability of schedule URLs

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,17 @@
-URLS = ["/", "/about", "/cfp", "/login", "/metrics"]
+URLS = [
+    "/",
+    "/about",
+    "/cfp",
+    "/login",
+    "/metrics",
+    "/schedule/2012",
+    "/schedule/2013",
+    "/schedule/2014",
+    "/schedule/2016",
+    "/schedule/2018",
+    "/schedule/2018/329-powerpoint-karaoke",
+    "/schedule/2016/306-using-printed-circuit-boards-to-make-snowflakes",
+]
 
 
 def test_url(client):
