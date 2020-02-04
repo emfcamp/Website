ed88d9b8bfaef558fcdacf9e852928fab4cf76ff
Jon Wood <jon@blankpad.net>
2020-02-04 21:26:54 +0000

    Filter by venue

--- js/schedule/App.js
+++ js/schedule/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
-import Schedule from './Schedule.js';
+import ScheduleData from './ScheduleData.js';
 
 function DateTimePicker({ value, onChange }) {
   let date = value.toISODate();
@@ -24,18 +24,120 @@ function DateTimePicker({ value, onChange }) {
   );
 }
 
+function Checkbox({ checked, onChange, children }) {
+  return (
+    <label className="checkbox"><input type="checkbox" checked={checked} onChange={ ev => onChange(ev.target.checked) } /> { children }</label>
+  );
+}
+
+function VenueSelector({ venues, selectedVenues, onChange }) {
+  function venueToggled(name, value) {
+    if (value) {
+      onChange([...selectedVenues, name]);
+    } else {
+      onChange(selectedVenues.filter(v => v !== name));
+    }
+  }
+
+  function selectOfficialVenues(ev) {
+    ev.preventDefault();
+    onChange(venues.filter(v => v.official).map(v => v.name));
+  }
+
+  function selectAll(ev) {
+    ev.preventDefault();
+    onChange([...venues.map(v => v.name)]);
+  }
+
+  function selectNone(ev) {
+    ev.preventDefault();
+    onChange([]);
+  }
+
+  function checkboxes() {
+    return venues.map(venue => {
+      return <Checkbox checked={ selectedVenues.indexOf(venue.name) !== -1 } key={ venue.name } onChange={ value => venueToggled(venue.name, value) }>{ venue.name }</Checkbox>;
+    });
+  }
+
+  return (
+    <>
+      <p>
+        <a href="#" onClick={ selectOfficialVenues }>Official Venues Only</a> | <a href="#" onClick={ selectAll }>Select All</a> | <a href="#" onClick={ selectNone }>Select None</a>
+      </p>
+      <div className="form-group form-inline">
+        { checkboxes() }
+      </div>
+    </>
+  );
+}
+
+function Event({ event, abbreviateTitles }) {
+  return (
+    <div className="schedule-event">
+      <h3 title={ event.title } className={ abbreviateTitles ? 'abbreviate-titles' : null }>{ event.title }</h3>
+      <p>{ event.startTime.toFormat('HH:mm') } to { event.endTime.toFormat('HH:mm') } | { event.venue }</p>
+    </div>
+  );
+}
+
+function Hour({ hour, content, abbreviateTitles }) {
+  if (content.length === 0) { return null; }
+
+  return (
+    <div className="schedule-hour">
+      <h2>{hour.toFormat('HH:mm')}</h2>
+      <div className="schedule-events-container">
+        { content.map(event => <Event key={event.id} event={event} abbreviateTitles={abbreviateTitles} />) }
+      </div>
+    </div>
+  );
+}
+
+function Calendar({ schedule, abbreviateTitles }) {
+  return schedule.hoursWithContent.map(hour => {
+    return (
+      <Hour key={hour.toISO()} hour={hour} content={schedule.contentForHour(hour)} abbreviateTitles={abbreviateTitles} />
+    );
+  });
+}
+
 function App() {
   const [year, setYear] = useState(2018);
-  const [mainEventsOnly, setMainEventsOnly] = useState(false);
   const [abbreviateTitles, setAbbreviateTitles] = useState(false);
   const [currentTime, setCurrentTime] = useState(DateTime.fromSQL('2018-08-31 09:00:00').setZone('Europe/London'));
+  const [selectedVenues, setSelectedVenues] = useState([]);
+
+  const [rawSchedule, setRawSchedule] = useState(null);
+  const [schedule, setSchedule] = useState(null);
+
+  // Pull the correct year's schedule if the year changes.
+  useEffect(() => {
+    fetch(`/schedule/${year}.json`)
+      .then(response => response.json())
+      .then(body => {
+        setRawSchedule(body)
+
+        let newSchedule = new ScheduleData(body, { currentTime });
+        setSchedule(newSchedule);
+        setSelectedVenues(newSchedule.venues.map(v => v.name));
+      });
+  }, [year]);
+
+  // Refilter the schedule if options change.
+  useEffect(() => {
+    if (rawSchedule == null) { return };
+
+    let newSchedule = new ScheduleData(rawSchedule, { currentTime, selectedVenues });
+    setSchedule(newSchedule);
+  }, [currentTime, selectedVenues]);
+
+  if (schedule === null) {
+    return <p>Loading...</p>;
+  }
 
   function yearChanged(ev) {
     setYear(ev.target.value);
-  }
-
-  function mainEventsChanged(ev) {
-    setMainEventsOnly(ev.target.checked);
   }
 
   function currentTimeChanged(newValue) {
@@ -45,27 +147,28 @@ function App() {
   return (
     <>
       <h1>Schedule</h1>
-      <p>
+      <h3>Venues</h3>
+      <VenueSelector venues={ schedule.venues } selectedVenues={ selectedVenues } onChange={ setSelectedVenues } />
+
+      <div className="form-group form-inline">
         <label htmlFor="scheduleYear">Year:</label>
-        <select onChange={yearChanged} id="scheduleYear" value={year}>
+        <select onChange={yearChanged} id="scheduleYear" value={year} className="form-control">
           <option>2012</option>
           <option>2014</option>
           <option>2016</option>
           <option>2018</option>
         </select>
-      </p>
-      <p>
-        <label><input type="checkbox" checked={mainEventsOnly} onChange={mainEventsChanged} /> Main events only</label>
-      </p>
-      <p>
-        <label><input type="checkbox" checked={abbreviateTitles} onChange={ ev => setAbbreviateTitles(ev.target.checked) } /> Abbreviate titles</label>
-      </p>
+      </div>
+
+      <div className="form-group form-inline">
+        <Checkbox checked={ abbreviateTitles } onChange={ setAbbreviateTitles }>Abbreviate titles</Checkbox>
+      </div>
       <p>
         <label>Current time:</label>
         <DateTimePicker value={currentTime} onChange={currentTimeChanged} />
       </p>
 
-      <Schedule year={ year } mainEventsOnly={ mainEventsOnly } currentTime={ currentTime } abbreviateTitles={ abbreviateTitles } />
+      <Calendar schedule={ schedule } />
     </>
   )
 }
--- js/schedule/ScheduleData.js
+++ js/schedule/ScheduleData.js
@@ -6,23 +6,35 @@ class ScheduleData {
     this.options = options;
     this.hoursWithContent = [];
     this.scheduleByHour = {};
+    this.venues = [];
+    this.venuesSeen = new Set();
 
     this.rawSchedule.forEach(row => {
       let e = this.parseEvent(row);
 
-      if (e.endTime > options.currentTime) {
-        let startHour = e.startTime.startOf('hour');
-        if (e.startTime <= options.currentTime) {
-          startHour = options.currentTime.startOf('hour');
-        }
-        let isoHour = startHour.toISO();
+      if (e.endTime <= options.currentTime) { return null; }
 
-        if (this.scheduleByHour[isoHour] === undefined) {
-          this.hoursWithContent.push(startHour);
-          this.scheduleByHour[isoHour] = [];
-        }
-        this.scheduleByHour[isoHour].push(e);
+      this.addVenue(e.venue, e.officialEvent);
+      this.venues = this.venues.sort((a,b) => {
+        if (a.official && !b.official) { return -1; }
+        if (!a.official && b.official) { return 1; }
+
+        return a.name.localeCompare(b.name);
+      });
+
+      if (options.selectedVenues && options.selectedVenues.indexOf(e.venue) === -1) { return null; }
+
+      let startHour = e.startTime.startOf('hour');
+      if (e.startTime <= options.currentTime) {
+        startHour = options.currentTime.startOf('hour');
       }
+      let isoHour = startHour.toISO();
+
+      if (this.scheduleByHour[isoHour] === undefined) {
+        this.hoursWithContent.push(startHour);
+        this.scheduleByHour[isoHour] = [];
+      }
+      this.scheduleByHour[isoHour].push(e);
     });
 
     Object.keys(this.scheduleByHour).forEach(hour => {
@@ -32,11 +44,15 @@ class ScheduleData {
     this.hoursWithContent = this.hoursWithContent.sort();
   }
 
+  addVenue(name, official) {
+    if (this.venuesSeen.has(name)) { return null; }
+
+    this.venuesSeen.add(name);
+    this.venues.push({ name: name, official: official });
+  }
+
   contentForHour(hour) {
     let events = this.scheduleByHour[hour.toISO()];
-    if (this.options.mainEventsOnly) {
-      events = events.filter(event => event.source === 'database');
-    }
 
     return events;
   }
@@ -47,6 +63,7 @@ class ScheduleData {
 
     e.startTime = DateTime.fromSQL(e.start_date, { locale: 'en-GB' });
     e.endTime = DateTime.fromSQL(e.end_date, { locale: 'en-GB' });
+    e.officialEvent = e.source === 'database';
 
     return e;
   }
