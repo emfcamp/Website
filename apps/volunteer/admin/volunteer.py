c8dbe4718625d628d038964190d75e6f13121b0e
Ian Norton <ian.norton@matillion.com>
2020-02-22 19:25:47 +0000

    Customise the volunteer list display columns

--- apps/volunteer/admin/volunteer.py
+++ apps/volunteer/admin/volunteer.py
@@ -5,10 +5,57 @@ from models.volunteer.volunteer import Volunteer
 
 
 class VolunteerUserModelView(VolunteerModelView):
-    column_searchable_list = ("nickname", "volunteer_email")
+    can_create = False
+    can_delete = False
+    can_set_page_size = True
+    can_view_details = True
+    column_details_exclude_list = ("user", "versions")
+    column_details_list = (
+        "nickname",
+        "volunteer_email",
+        "volunteer_phone",
+        "planned_arrival",
+        "planned_departure",
+        "missing_shifts_opt_in",
+        "over_18",
+        "allow_comms_during_event",
+        "banned",
+    )
     column_filters = ["trained_roles", "allow_comms_during_event"]
+    column_list = (
+        "nickname",
+        "volunteer_email",
+        "planned_arrival",
+        "planned_departure",
+        "banned",
+    )
+    column_searchable_list = ("nickname", "volunteer_email")
+    details_modal = True
+    edit_modal = True
+    form_columns = (
+        "nickname",
+        "volunteer_email",
+        "volunteer_phone",
+        "planned_arrival",
+        "planned_departure",
+
+        "interested_roles",
+        "trained_roles",
+
+        "missing_shifts_opt_in",
+        "over_18",
+        "allow_comms_during_event",
+        "banned",
+    )
+    form_excluded_columns = ("user", "versions")
+    page_size = 50  # the number of entries to display on the list view
 
 
+# Add menu item Volunteers
 volunteer_admin.add_view(
-    VolunteerUserModelView(Volunteer, db.session, category="volunteers")
+    VolunteerUserModelView(
+        Volunteer,
+        db.session,
+        name="Volunteers"
+    )
 )
