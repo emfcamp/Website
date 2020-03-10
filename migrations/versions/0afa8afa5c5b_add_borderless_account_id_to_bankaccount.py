56db4dc1cebed4d3b877318578b0b0af42ba9ab2
James Addison <james@reciperadar.com>
2020-03-10 16:22:05 +0000

    Revert "Post-merge conflict: Revise down_revision for alembic migration"
    This reverts commit ee22bf4d5fc744e049a529b4dd84872953c7b12a.

--- migrations/versions/0afa8afa5c5b_add_borderless_account_id_to_bankaccount.py
+++ migrations/versions/0afa8afa5c5b_add_borderless_account_id_to_bankaccount.py
@@ -1,14 +1,14 @@
 """Add borderless_account_id to BankAccount
 
 Revision ID: 0afa8afa5c5b
-Revises: fa044cbcbf79
+Revises: d01b87ff85d2
 Create Date: 2020-03-04 13:54:43.683627
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '0afa8afa5c5b'
-down_revision = 'fa044cbcbf79'
+down_revision = 'd01b87ff85d2'
 
 from alembic import op
 import sqlalchemy as sa
