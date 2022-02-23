34cf139e84e2ae74a52decd75454e8936c171941
Mark Steward <marksteward@gmail.com>
2022-02-23 22:57:09 +0000

    Send to the right channel

--- docker/startup-irc-notify.sh
+++ docker/startup-irc-notify.sh
@@ -8,7 +8,7 @@ MIGRATION_GRACE_SECS=${MIGRATION_GRACE_SECS:-90}
 
 send() {
   echo "Sending IRC notification: $1"
-  echo "$1" | curl -s -m5 telnet://$IRCCAT ||:
+  echo "#emfcamp-web $1" | curl -s -m5 telnet://$IRCCAT ||:
 }
 
 db_head=$(poetry run flask db current)
