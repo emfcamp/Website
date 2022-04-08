10582119777de1f63043b44edd0e0d19f68c62e5
Robert McWilliam <rmcw@allmail.net>
2022-04-09 00:43:08 +0100

    Make irc_send silent when env variable isn't set and add timeout

--- apps/common/irc.py
+++ apps/common/irc.py
@@ -5,11 +5,11 @@ from flask import current_app as app
 
 def irc_send(message):
     if "IRCCAT" not in os.environ:
-        app.logger.warn("No IRCCAT env variable?")
         return
     host, port = os.environ.get("IRCCAT").split(":")
     port = int(port)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
     try:
         s.connect((host, port))
         s.sendall(message.encode() + b"\n")
