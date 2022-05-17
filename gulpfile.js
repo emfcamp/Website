1b6959e2d961ea8606f43951144ac4bbff8c4ed5
Russ Garrett <russ@garrett.co.uk>
2022-05-17 22:28:50 +0100

    Restore scheduler js for admin

--- gulpfile.js
+++ gulpfile.js
@@ -91,10 +91,13 @@ const main_js = (cb) => pump(jsBuild('main.js'), cb),
   line_up_js = (cb) => pump(jsBuild('line-up.js'), cb),
   schedule_js = (cb) => pump(jsBuild('schedule.js'), cb),
   volunteer_schedule_js = (cb) => pump(jsBuild('volunteer-schedule.js'), cb),
-  arrivals_js = (cb) => pump(jsBuild('arrivals.js'), cb);
+  arrivals_js = (cb) => pump(jsBuild('arrivals.js'), cb),
+  dhtmlx_scheduler_js = (cb) => pump(jsBuild('dhtmlx_scheduler.js'), cb),
+  dhtmlxscheduler_all_timed = (cb) => pump(jsBuild('dhtmlxscheduler_all_timed.js'), cb);
+
 
 function js(cb) {
-  gulp.parallel(main_js, line_up_js, schedule_js, volunteer_schedule_js, arrivals_js)(cb);
+  gulp.parallel(main_js, line_up_js, schedule_js, volunteer_schedule_js, arrivals_js, dhtmlx_scheduler_js, dhtmlxscheduler_all_timed)(cb);
 }
 
 function css(cb) {
@@ -108,6 +111,7 @@ function css(cb) {
       'css/schedule.scss',
       'css/volunteer_schedule.scss',
       'css/flask-admin.scss',
+      'css/dhtmlxscheduler_flat.css',
     ]),
     gulpif(!production, sourcemaps.init()),
     sass({ includePaths: ['../node_modules'] }).on('error', function (err) {
