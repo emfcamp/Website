f61980eb1d98be9eeff323007ebffe4183b34889
Jon Wood <jon@blankpad.net>
2022-05-31 10:57:19 +0100

    API endpoint to list installations

---
+++ apps/api/installations.py
@@ -0,0 +1,38 @@
+from flask import url_for
+from flask_restful import Resource
+from models import event_year
+from models.cfp import InstallationProposal
+from geoalchemy2.shape import to_shape
+
+from . import api
+
+
+def render_installation(installation: InstallationProposal):
+    return {
+        "id": installation.id,
+        "name": installation.title,
+        "url": url_for(
+            "schedule.item",
+            year=event_year(),
+            proposal_id=installation.id,
+            _external=True,
+        ),
+        "description": installation.description,
+        "location": to_shape(installation.scheduled_venue.location).__geo_interface__
+        if installation.scheduled_venue and installation.scheduled_venue.location
+        else None,
+    }
+
+
+class Installations(Resource):
+    def get(self):
+        result = []
+        proposals = InstallationProposal.query.filter(
+            InstallationProposal.state.in_(["accepted", "finished"])
+        ).all()
+        for proposal in proposals:
+            result.append(render_installation(proposal))
+        return result
+
+
+api.add_resource(Installations, "/installations")
