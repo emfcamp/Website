c7da5f38d9e3178b14ed90495c5e7af38d4d0f25
Russ Garrett <russ@garrett.co.uk>
2023-09-19 11:53:27 +0100

    Add commented-out faker logic

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,7 +1,13 @@
 from apps.base.tasks_export import get_export_data
 
+# from apps.base.dev.fake import FakeDataGenerator
+
 
 def test_export(app):
     """Test that the export succeeds without error."""
+    # Generating fake data will improve coverage but takes ages
+    #
+    # fdg = FakeDataGenerator()
+    # fdg.run()
     export = list(get_export_data())
     assert len(export) > 0
