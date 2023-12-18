2085434f3bea7174a7b13f47db4ebfda862236d9
James Addison <jay@jp-hosting.net>
2023-12-18 16:52:44 +0000

    payments: expand test coverage for ISO-11649 header filtering

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -7,9 +7,14 @@ from models.payment import BankTransaction
     "ref, expected",
     [
         ("M87X CJ3Q", "M87X CJ3Q"),
+        ("prefix*M87X CJ3Q*suffix", "prefix*M87X CJ3Q*suffix"),
         ("RF91 M87X CJ3Q", "M87X CJ3Q"),
+        ("name value RF52RF23MHBY type value", "name value RF23MHBY type value"),
+        ("prefix*RF52RF23MHBY*suffix", "prefix*RF23MHBY*suffix"),
         ("RF52RF23MHBY", "RF23MHBY"),
+        ("RF33*RF52RF23MHBY*RF33", "RF33*RF23MHBY*RF33"),
         ("RF23MHBY", "RF23MHBY"),
+        ("RF33*RF23MHBY*RF33", "RF33*RF23MHBY*RF33"),
     ],
 )
 def test_trim_iso11649_header(ref, expected):
