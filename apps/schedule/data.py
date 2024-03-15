5afdd90379cf688af0b94130fa83caba79d1ae6f
Sam Willcocks <sam@wlcx.cc>
2024-03-15 23:00:50 +0000

    cfp/schedule: Add video recording lost flag

--- apps/schedule/data.py
+++ apps/schedule/data.py
@@ -52,6 +52,7 @@ def _get_proposal_dict(proposal: Proposal, favourites_ids):
         video_res["youtube"] = proposal.youtube_url
     if proposal.thumbnail_url:
         video_res["preview_image"] = proposal.thumbnail_url
+    video_res["recording_lost"] = proposal.video_recording_lost
     if video_res:
         res["video"] = video_res
     return res
