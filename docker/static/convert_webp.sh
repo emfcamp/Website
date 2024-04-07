d1504b38883e58540e69230c7931b45a97020ec8
Russ Garrett <russ@garrett.co.uk>
2024-04-07 16:53:58 +0100

    Fix webp serving

--- docker/static/convert_webp.sh
+++ docker/static/convert_webp.sh
@@ -4,7 +4,7 @@ set -e
 # Convert png/jpeg files to webp, without re-converting existing webp files.
 
 find "$1" -type f \( -iname "*.jpg" -o -iname "*.png" \) | while read -r file; do
-    webp_file="${file%.*}.webp"
+    webp_file="$file.webp"
     if [ ! -f "$webp_file" ]; then
         cwebp -quiet "$file" -o "$webp_file"
     fi
