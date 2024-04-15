9e386c8e2c2b5f9ae500d911ff572f16237d44a6
Jon Wood <jon@blankpad.net>
2024-04-15 19:50:53 +0000

    Button to send a push notification

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,12 +1,12 @@
 from wtforms import SubmitField, BooleanField
 from apps.common import json_response
 from main import db
-from flask import render_template, request, current_app as app
+from flask import render_template, request, current_app as app, flash, redirect, url_for
 from flask_login import current_user, login_required
 
 from . import notifications
 from ..common.forms import Form
-from models.web_push import public_key, WebPushTarget
+from models.web_push import public_key, WebPushTarget, notify
 from models.notifications import UserNotificationPreference
 
 
@@ -35,6 +35,20 @@ def index():
     return render_template(
         "notifications/index.html", public_key=public_key(), form=form
     )
+
+
+@notifications.route("/test", methods=["POST"])
+@login_required
+def test():
+    if len(current_user.web_push_targets) == 0:
+        flash("You have no devices configured for push notifications.")
+        return redirect(url_for("notifications.index"))
+
+    for target in current_user.web_push_targets:
+        notify(target, "This is a test notification.")
+
+    flash("Test notifications have been sent.")
+    return redirect(url_for("notifications.index"))
 
 
 @notifications.route("/register", methods=["POST"])
