f4c74f5b74c4adc3d053b25df2cc92f9b637145d
Mattsi <mattsi.jansky@codurance.com>
2024-04-27 16:58:20 +0100

    Feature flag the jobs board page

--- apps/jobs_board/__init__.py
+++ apps/jobs_board/__init__.py
@@ -5,9 +5,12 @@ from flask import (
     Blueprint,
 )
 
+from ..common import feature_flag
+
 jobs_board = Blueprint("jobs_board", __name__)
 
 @jobs_board.route("/")
+@feature_flag("JOBS_BOARD")
 def main():
     with open('apps/jobs_board/jobs.json', 'r') as file:
         jobs = json.load(file)
