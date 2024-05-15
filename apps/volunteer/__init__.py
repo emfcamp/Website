3b382be6cc0f1135c4c1c37e6bb863561bd2af25
Mark Steward <marksteward@gmail.com>
2024-05-15 18:21:41 +0100

    Redirect to volunteer sign-up if appropriate

--- apps/volunteer/__init__.py
+++ apps/volunteer/__init__.py
@@ -1,14 +1,32 @@
-from flask import Blueprint, request
+from decorator import decorator
 
-from ..common import require_permission
+from flask import Blueprint, request, current_app as app, redirect, url_for, abort
+from flask_login import current_user
+
+from models.volunteer import Volunteer
 
 volunteer = Blueprint("volunteer", __name__)
 
+
+# This is like require_permission but redirects to volunteer sign-up
+def require_volunteer_permission(permission):
+    def call(f, *args, **kwargs):
+        if current_user.is_authenticated:
+            if not Volunteer.get_for_user(current_user):
+                return redirect(url_for("volunteer.sign_up", next=request.path))
+            if current_user.has_permission(permission):
+                return f(*args, **kwargs)
+            abort(404)
+        return app.login_manager.unauthorized()
+
+    return decorator(call)
+
+
 # User basically means they have signed up
 # These aren't needed for the admin views as they have their auth baked in.
-v_user_required = require_permission("volunteer:user")
-v_admin_required = require_permission("volunteer:admin")
-v_manager_required = require_permission("volunteer:manager")
+v_user_required = require_volunteer_permission("volunteer:user")
+v_admin_required = require_volunteer_permission("volunteer:admin")
+v_manager_required = require_volunteer_permission("volunteer:manager")
 
 
 @volunteer.context_processor
