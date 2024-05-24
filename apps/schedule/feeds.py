afb5861cea78b3ace8cb360be5b12f41f17c2bf2
Franziska Kunsmann <hi@kunsmann.eu>
2024-05-24 12:26:27 +0200

    schedule/feeds: make json feed schema compatible

--- apps/schedule/feeds.py
+++ apps/schedule/feeds.py
@@ -1,10 +1,13 @@
 import json
+from datetime import timedelta
 from icalendar import Calendar, Event
 from flask import request, abort, current_app as app, Response
 from flask_cors import cross_origin
 from flask_login import current_user
+from math import ceil
 
-from models import event_year
+from main import external_url
+from models import event_year, event_start, event_end
 from models.user import User
 from models.cfp import Proposal
 
@@ -17,7 +20,7 @@ from .data import (
     _convert_time_to_str,
     _get_upcoming,
 )
-from . import schedule
+from . import event_tz, schedule
 
 
 def _format_event_description(event):
@@ -54,6 +57,136 @@ def schedule_json(year):
 
     # NB this is JSON in a top-level array (security issue for low-end browsers)
     return Response(json.dumps(schedule), mimetype="application/json")
+
+
+@schedule.route("/schedule/schedule-<int:year>.json") # TODO validate url with upstream
+@cross_origin(methods=["GET"])
+def schedule_json_schema(year):
+    if year != event_year():
+        return feed_historic(year, "json")
+
+    if not feature_enabled('SCHEDULE'):
+        abort(404)
+
+    def duration_hhmm(duration_minutes):
+        if not duration_minutes or duration_minutes < 1:
+            return "00:00"
+        return "{}:{}".format(
+            int(duration_minutes/60),
+            str(duration_minutes%60).zfill(2),
+        )
+
+    schedule = (
+        Proposal.query.filter(
+            Proposal.is_accepted,
+            Proposal.scheduled_time.isnot(None),
+            Proposal.scheduled_venue_id.isnot(None),
+            Proposal.scheduled_duration.isnot(None),
+        )
+        .order_by(Proposal.scheduled_time)
+        .all()
+    )
+
+    duration_days = ceil((event_end() - event_end()).total_seconds / 86400),
+
+    rooms = [
+        proposal.scheduled_venue.name
+        for proposal in schedule
+    ]
+
+    schedule_json = {
+        "version": "1.0-public",
+        "conference": {
+            "acronym": "emf{}".format(event_year()),
+            "days": [],
+            "daysCount": duration_days,
+            "end": event_end().strftime("%Y-%m-%d"),
+            "rooms": [
+                {
+                    "name": room,
+                }
+                for room in rooms
+            ],
+            "start": event_start().strftime("%Y-%m-%d"),
+            "time_zone_name": event_tz,
+            "timeslot_duration": "00:10",
+            "title": "Electromagnetic Field {}".format(event_year()),
+            "url": external_url("main"),
+        },
+    }
+
+    for day in range(0, duration_days):
+        day_dt = event_start() + timedelta(days=day)
+        day_schedule = {
+            "date": day_dt.strftime("%Y-%m-%d"),
+            "day_end": (day_dt.replace(hour=3, minute=59, second=59) + timedelta(days=1)).isoformat(),
+            "day_start": day_dt.replace(hour=4, minute=0, second=0).isoformat(),
+            "index": day,
+            "rooms": {},
+        }
+        for room in rooms:
+            day_schedule["rooms"][room] = []
+            for proposal in schedule:
+                if proposal.scheduled_venue.name != room:
+                    # TODO find a better way to do that
+                    continue
+                links = {
+                    proposal.c3voc_url,
+                    proposal.youtube_url,
+                    proposal.thumbnail_url,
+                    proposal.map_link,
+                }
+                links.discard(None)
+                links.discard("")
+                day_schedule["rooms"][room].append({
+                    "abstract": None, # The proposal model does not implement abstracts
+                    "attachments": [],
+                    "date": event_tz.localize(proposal.start_date).isoformat(),
+                    "description": proposal.description,
+                    "do_not_record": False if proposal.may_record else True,
+                    "duration": duration_hhmm(proposal.duration_minutes),
+                    "guid": None,
+                    "id": proposal.id,
+                    # This assumes there will never be a non-english talk,
+                    # which is probably fine for a conference in the UK.
+                    "language": "en",
+                    "links": sorted(links),
+                    "persons": [
+                        {
+                            "name": name.strip(),
+                            "public_name": name.strip(),
+                        }
+                        for name in (proposal.published_names or proposal.user.name).split(",")
+                    ],
+                    "recording_license": "CC BY-SA 3.0",
+                    "room": room,
+                    "slug": "emf{}-{}-{}".format(
+                        event_year(),
+                        proposal.id,
+                        proposal.slug,
+                    ),
+                    "start": event_tz.localize(proposal.start_date).strftime("%H:%M"),
+                    "subtitle": None,
+                    "title": proposal.display_title,
+                    "track": None, # TODO does emf have tracks?
+                    "type": proposal.type,
+                    "url": external_url(
+                        ".item",
+                        year=event_year(),
+                        proposal_id=proposal.id,
+                        slug=proposal.slug,
+                    ),
+                })
+        schedule_json["conference"]["days"].append(day_schedule)
+
+    return Response(json.dumps({
+        "schedule": schedule_json,
+        "$schema": "https://c3voc.de/schedule/schema.json",
+        "generator": {
+            "name": "emfcamp-website",
+            "url": "https://github.com/emfcamp/Website",
+        },
+    }), mimetype="application/json")
 
 
 @schedule.route("/schedule/<int:year>.frab")
