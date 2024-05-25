831eb15decf793ecd58390364c7e8caa3d58a24d
Andrew Mulholland <833193+gbaman@users.noreply.github.com>
2024-05-25 14:15:08 +0100

    Initial cleanup of ages
    First pass for only ages not currently in use for 2024. Ideally to remove other ages for 2026.

--- models/cfp.py
+++ models/cfp.py
@@ -110,21 +110,16 @@ AGE_RANGE_OPTIONS = [
         "Aimed at adults, but supervised kids welcome.",
         "Aimed at adults, but supervised kids welcome.",
     ),
-    ("3+", "3+"),
-    ("4+", "4+"),
     ("5+", "5+"),
-    ("6+", "6+"),
-    ("7+", "7+"),
+    ("6+", "6+"), # TODO: Remove in future to simplify ages
+    ("7+", "7+"), # TODO: Remove in future to simplify ages
     ("8+", "8+"),
-    ("9+", "9+"),
     ("10+", "10+"),
-    ("11+", "11+"),
+    ("11+", "11+"), # TODO: Remove in future to simplify ages
     ("12+", "12+"),
-    ("13+", "13+"),
     ("14+", "14+"),
-    ("15+", "15+"),
     ("16+", "16+"),
-    ("17+", "17+"),
+    ("17+", "17+"), # TODO: Remove in future to simplify ages
     ("18+", "18+"),
 ]
 
