37bc49f96b87839a9fcdfd70134f1f0e14412a9e
Sam <gitsam@samlr.com>
2024-05-31 14:53:55 +0100

    Move installations to 2024 sub-path
    we'll want to be able to reuse these paths in other years

--- apps/base/installations.py
+++ apps/base/installations.py
@@ -2,16 +2,21 @@
     Pages under /installations - the Installations programme
 """
 
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from . import base
+from models import event_year
 from apps.common import render_markdown
 
-
-@base.route("/installations/<page_name>")
-def installations_page(page_name: str):
-    return render_markdown(f"installations/{page_name}", template="installations/template.html", page_name=page_name)
-
 @base.route("/installations")
-def installations():
-    return render_template("installations/index.html")
+def yearly_installation_redirection():
+    return redirect(url_for('.installations', year=event_year()))
+
+
+@base.route("/installations/<int:year>/<page_name>")
+def installations_page(year: int, page_name: str):
+    return render_markdown(f"installations/{year}/{page_name}", template=f"installations/{year}/template.html", page_name=page_name)
+
+@base.route("/installations/<int:year>")
+def installations(year: int):
+    return render_template(f"installations/{year}/main.html")
