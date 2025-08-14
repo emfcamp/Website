bbb8ef52d7f242996a78f9118af646fc23b543a0
Sam Willcocks <sam@wlcx.cc>
2025-08-14 17:57:00 +0200

    admin: move products and product views to blueprints
    - Changes product views urls from /product_views to /product-views
    - Change a bunch of url_for view names to be absolute. This was
      necessary after the 2 new blueprints created namespaces under admin,
      but also just seems like a better idea long term

--- apps/admin/products.py
+++ apps/admin/products.py
@@ -7,6 +7,7 @@ from flask import (
     abort,
     url_for,
     current_app as app,
+    Blueprint,
 )
 from flask_login import current_user
 
@@ -33,7 +34,6 @@ from ..common.email import (
     format_trusted_plaintext_email,
     from_email,
 )
-from . import admin
 from .forms import (
     EditProductForm,
     NewProductForm,
@@ -63,13 +63,16 @@ def get_user_purchases(query):
     )
 
 
-@admin.route("/products")
-def products():
+products = Blueprint("products", __name__)
+
+
+@products.route("/")
+def products_main():
     root_groups = ProductGroup.query.filter_by(parent_id=None).order_by(ProductGroup.id).all()
     return render_template("admin/products/overview.html", root_groups=root_groups)
 
 
-@admin.route("/products/<int:product_id>/edit", methods=["GET", "POST"])
+@products.route("/<int:product_id>/edit", methods=["GET", "POST"])
 def edit_product(product_id):
     form = EditProductForm()
 
@@ -84,13 +87,13 @@ def edit_product(product_id):
     return render_template("admin/products/edit-product.html", product=product, form=form)
 
 
-@admin.route(
-    "/products/group/<int:parent_id>/new",
+@products.route(
+    "/group/<int:parent_id>/new",
     defaults={"copy_id": None},
     methods=["GET", "POST"],
 )
-@admin.route(
-    "/products/<int:copy_id>/clone",
+@products.route(
+    "/<int:copy_id>/clone",
     defaults={"parent_id": None},
     methods=["GET", "POST"],
 )
@@ -123,7 +126,7 @@ def new_product(copy_id, parent_id):
     return render_template("admin/products/new-product.html", parent=parent, product_id=copy_id, form=form)
 
 
-@admin.route("/products/<int:product_id>")
+@products.route("/<int:product_id>")
 def product_details(product_id):
     product = Product.query.get_or_404(product_id)
     user_purchases = get_user_purchases(PriceTier.query.filter_by(product_id=product_id))
@@ -135,7 +138,7 @@ def product_details(product_id):
     )
 
 
-@admin.route("/products/<int:product_id>/new-tier", methods=["GET", "POST"])
+@products.route("/<int:product_id>/new-tier", methods=["GET", "POST"])
 def new_price_tier(product_id):
     form = NewPriceTierForm()
     product = Product.query.get_or_404(product_id)
@@ -160,7 +163,7 @@ def new_price_tier(product_id):
     return render_template("admin/products/price-tier-new.html", product=product, form=form)
 
 
-@admin.route("/products/price-tiers/<int:tier_id>")
+@products.route("/price-tiers/<int:tier_id>")
 def price_tier_details(tier_id):
     tier = PriceTier.query.get_or_404(tier_id)
     form = ModifyPriceTierForm()
@@ -173,7 +176,7 @@ def price_tier_details(tier_id):
     )
 
 
-@admin.route("/products/price-tiers/<int:tier_id>", methods=["POST"])
+@products.route("/price-tiers/<int:tier_id>", methods=["POST"])
 def price_tier_modify(tier_id):
     form = ModifyPriceTierForm()
     tier = PriceTier.query.get_or_404(tier_id)
@@ -203,7 +206,7 @@ def price_tier_modify(tier_id):
     return abort(401)
 
 
-@admin.route("/products/price-tiers/<int:tier_id>/edit", methods=["GET", "POST"])
+@products.route("/price-tiers/<int:tier_id>/edit", methods=["GET", "POST"])
 def price_tier_edit(tier_id):
     tier = PriceTier.query.get_or_404(tier_id)
     form = EditPriceTierForm(obj=tier)
@@ -235,7 +238,7 @@ def price_tier_edit(tier_id):
     return render_template("admin/products/price-tier-edit.html", tier=tier, form=form)
 
 
-@admin.route("/products/group/<int:group_id>")
+@products.route("/group/<int:group_id>")
 def product_group_details(group_id):
     group = ProductGroup.query.get_or_404(group_id)
 
@@ -252,7 +255,7 @@ def product_group_details(group_id):
     )
 
 
-@admin.route("/products/group/new", methods=["GET", "POST"])
+@products.route("/group/new", methods=["GET", "POST"])
 def product_group_new():
     if request.args.get("parent"):
         parent = ProductGroup.query.get_or_404(request.args.get("parent"))
@@ -274,12 +277,12 @@ def product_group_new():
         db.session.add(pg)
         db.session.commit()
         flash("ProductGroup created")
-        return redirect(url_for(".product_group_details", group_id=pg.id))
+        return redirect(url_for("admin.products.product_group_details", group_id=pg.id))
 
     return render_template("admin/products/product-group-edit.html", method="new", parent=parent, form=form)
 
 
-@admin.route("/products/group/<int:group_id>/edit", methods=["GET", "POST"])
+@products.route("/group/<int:group_id>/edit", methods=["GET", "POST"])
 def product_group_edit(group_id):
     group = ProductGroup.query.get_or_404(group_id)
     form = EditProductGroupForm()
@@ -288,14 +291,14 @@ def product_group_edit(group_id):
         db.session.add(group)
         db.session.commit()
         flash("ProductGroup updated")
-        return redirect(url_for(".product_group_details", group_id=group.id))
+        return redirect(url_for("admin.products.product_group_details", group_id=group.id))
 
     form.init_with_pg(group)
 
     return render_template("admin/products/product-group-edit.html", method="edit", group=group, form=form)
 
 
-@admin.route("/products/group/<int:group_id>/copy", methods=["GET", "POST"])
+@products.route("/group/<int:group_id>/copy", methods=["GET", "POST"])
 def product_group_copy(group_id):
     group = ProductGroup.query.get_or_404(group_id)
     form = CopyProductGroupForm()
@@ -351,12 +354,12 @@ def product_group_copy(group_id):
         db.session.add(new_group)
         db.session.commit()
         flash("ProductGroup copied")
-        return redirect(url_for(".product_group_details", group_id=new_group.id))
+        return redirect(url_for("admin.products.product_group_details", group_id=new_group.id))
 
     return render_template("admin/products/product-group-copy.html", group=group, form=form)
 
 
-@admin.route("/tees")
+@products.route("/tees")
 def tees():
     purchases = (
         ProductGroup.query.filter_by(type="tees")
@@ -370,8 +373,11 @@ def tees():
     return render_template("admin/products/tee-purchases.html", purchases=purchases)
 
 
-@admin.route("/product_views")
-def product_views():
+product_views = Blueprint("product_views", __name__)
+
+
+@product_views.route("/")
+def product_views_main():
     view_counts = (
         ProductView.query.outerjoin(ProductView.product_view_products)
         .with_entities(ProductView, func.count(ProductViewProduct.view_id))
@@ -382,7 +388,7 @@ def product_views():
     return render_template("admin/products/views.html", view_counts=view_counts)
 
 
-@admin.route("/product_views/new", methods=["GET", "POST"])
+@product_views.route("/new", methods=["GET", "POST"])
 def product_view_new():
     form = NewProductViewForm()
 
@@ -397,12 +403,12 @@ def product_view_new():
         db.session.add(view)
         db.session.commit()
         flash("ProductView created")
-        return redirect(url_for(".product_view", view_id=view.id))
+        return redirect(url_for("admin.products.product_view", view_id=view.id))
 
     return render_template("admin/products/view-new.html", form=form)
 
 
-@admin.route("/product_views/<int:view_id>", methods=["GET", "POST"])
+@product_views.route("/<int:view_id>", methods=["GET", "POST"])
 def product_view(view_id):
     view = ProductView.query.get_or_404(view_id)
 
@@ -451,10 +457,10 @@ def product_view(view_id):
     return render_template("admin/products/view-edit.html", view=view, form=form, voucher_stats=stats)
 
 
-@admin.route("/product_views/<int:view_id>/add", methods=["GET", "POST"])
-@admin.route("/product_views/<int:view_id>/add/<int:group_id>", methods=["GET", "POST"])
-@admin.route(
-    "/product_views/<int:view_id>/add/<int:group_id>/<int:product_id>",
+@product_views.route("/<int:view_id>/add", methods=["GET", "POST"])
+@product_views.route("/<int:view_id>/add/<int:group_id>", methods=["GET", "POST"])
+@product_views.route(
+    "/<int:view_id>/add/<int:group_id>/<int:product_id>",
     methods=["GET", "POST"],
 )
 def product_view_add(view_id, group_id=None, product_id=None):
@@ -497,7 +503,7 @@ def product_view_add(view_id, group_id=None, product_id=None):
     )
 
 
-@admin.route("/product_views/<int:view_id>/voucher")
+@product_views.route("/<int:view_id>/voucher")
 def product_view_voucher_list(view_id):
     view = ProductView.query.get_or_404(view_id)
     vouchers = Voucher.query.filter_by(view=view)
@@ -513,7 +519,7 @@ def product_view_voucher_list(view_id):
     return render_template("admin/products/view-vouchers.html", view=view, vouchers=vouchers)
 
 
-@admin.route("/product_views/<int:view_id>/voucher/<string:voucher_code>")
+@product_views.route("/<int:view_id>/voucher/<string:voucher_code>")
 def product_view_voucher_detail(view_id, voucher_code):
     view = ProductView.query.get_or_404(view_id)
     voucher = Voucher.get_by_code(voucher_code)
@@ -522,7 +528,7 @@ def product_view_voucher_detail(view_id, voucher_code):
     return render_template("admin/products/view-voucher.html", view=view, voucher=voucher)
 
 
-@admin.route("/product_views/<int:view_id>/voucher/add", methods=["GET", "POST"])
+@product_views.route("/<int:view_id>/voucher/add", methods=["GET", "POST"])
 def product_view_add_voucher(view_id):
     view = ProductView.query.get_or_404(view_id)
     form = NewVoucherForm()
@@ -539,7 +545,7 @@ def product_view_add_voucher(view_id):
         flash("Voucher successfully created")
         return redirect(
             url_for(
-                ".product_view_voucher_detail",
+                "admin.product_views.product_view_voucher_detail",
                 view_id=view_id,
                 voucher_code=voucher.code,
             )
@@ -548,8 +554,8 @@ def product_view_add_voucher(view_id):
     return render_template("admin/products/view-add-voucher.html", view=view, form=form)
 
 
-@admin.route(
-    "/product_views/<int:view_id>/voucher/<string:voucher_code>/edit",
+@product_views.route(
+    "/<int:view_id>/voucher/<string:voucher_code>/edit",
     methods=["GET", "POST"],
 )
 def product_view_edit_voucher(view_id, voucher_code):
@@ -565,7 +571,7 @@ def product_view_edit_voucher(view_id, voucher_code):
         flash("Voucher updated")
         return redirect(
             url_for(
-                ".product_view_voucher_detail",
+                "admin.products.product_view_voucher_detail",
                 view_id=view_id,
                 voucher_code=voucher_code,
             )
@@ -576,7 +582,7 @@ def product_view_edit_voucher(view_id, voucher_code):
     return render_template("admin/products/view-edit-voucher.html", view=view, voucher=voucher, form=form)
 
 
-@admin.route("/product_views/<int:view_id>/voucher/bulk_add", methods=["GET", "POST"])
+@product_views.route("/<int:view_id>/voucher/bulk_add", methods=["GET", "POST"])
 def product_view_bulk_add_vouchers_by_email(view_id):
     view = ProductView.query.get_or_404(view_id)
     form = BulkVoucherEmailForm()
