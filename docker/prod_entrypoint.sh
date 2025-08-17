628a79c2fcf87abc63efef06524f59e86667159c
Russ Garrett <russ@garrett.co.uk>
2025-08-17 22:40:59 +0100

    s/poetry/uv/ in a few places

--- docker/prod_entrypoint.sh
+++ docker/prod_entrypoint.sh
@@ -16,4 +16,4 @@ $PSQL emf_site -c 'CREATE EXTENSION postgis' || true
 
 mkdir -p $PROMETHEUS_MULTIPROC_DIR
 
-exec poetry run gunicorn -k gthread -c gunicorn.py -w 6 -b '0.0.0.0:8000' --preload wsgi:app
+exec uv run gunicorn -k gthread -c gunicorn.py -w 6 -b '0.0.0.0:8000' --preload wsgi:app
