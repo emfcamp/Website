dc6e61192348b4c5301c48ed1a0831220f1ad14c
amx109 <amx109@gmail.com>
2026-03-03 23:06:13 +0000

    change the way the code of conduct is rendered, now via markdown. also has sub pages for transparency reports

--- apps/base/__init__.py
+++ apps/base/__init__.py
@@ -156,12 +156,6 @@ def sponsor():
 def badge():
     return redirect("https://tildagon.badge.emfcamp.org/")
 
-
-@base.route("/code-of-conduct")
-def code_of_conduct():
-    return render_template("code-of-conduct.html")
-
-
 @base.route("/googlec108e6ab4f75019d.html")
 def google_verification_russ():
     return "google-site-verification: googlec108e6ab4f75019d.html"
@@ -220,3 +214,4 @@ from . import tasks_banking  # noqa
 from . import tasks_export  # noqa
 from . import tasks_videos  # noqa
 from . import dev  # noqa
+from . import code_of_conduct
