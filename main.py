9ebbcb975abc850dbd56c39b39bdfa7582551e9b
Russ Garrett <russ@garrett.co.uk>
2026-03-03 12:12:34 +0000

    Revert "Enable content security policy in production"
    This reverts commit d418e7d8a9591a57f3f7eae030a0c96bcab269ea.

--- main.py
+++ main.py
@@ -294,7 +294,9 @@ def create_app(dev_server=False, config_override=None):
             csp["script-src"].append(f"'nonce-{g.csp_nonce}'")
 
         value = "; ".join(k + " " + " ".join(v) for k, v in csp.items())
-        response.headers["Content-Security-Policy"] = value
+
+        if app.config.get("DEBUG"):
+            response.headers["Content-Security-Policy"] = value
 
         return response
 
