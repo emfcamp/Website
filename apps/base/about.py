424e2a8be2697ccd35969b757bd28518e8006321
Russ Garrett <russ@garrett.co.uk>
2026-03-11 12:22:18 +0000

    Move some pages to new organisation section

--- apps/base/about.py
+++ apps/base/about.py
@@ -5,8 +5,6 @@ Content about EMF the organisation should go in /organisation (organisation.py),
 although some legacy content remains here.
 """
 
-import json
-
 from flask import (
     redirect,
     render_template,
@@ -34,20 +32,6 @@ def page(page_name: str) -> ResponseValue:
     return render_markdown(f"about/{page_name}", page_name=page_name)
 
 
-@base.route("/about/diversity/<int:year>")
-def yearly_diversity_stats(year: int) -> ResponseValue:
-    if year in (2018, 2022):
-        with open(f"exports/{year}/public/UserDiversity.json") as raw_data:
-            data = json.load(raw_data)
-
-        return render_template(
-            "about/diversity/pre-2024-stats.html",
-            year=year,
-            data=data["diversity"],
-        )
-    return render_markdown(f"about/diversity/{year}")
-
-
 # About and Contact have actual logic in them, so remain as HTML rather than
 # markdown
 @base.route("/about")
@@ -55,16 +39,29 @@ def about() -> ResponseValue:
     return render_template("about/index.html")
 
 
-@base.route("/company")
-def company() -> ResponseValue:
-    return render_markdown("about/company")
-
-
 @base.route("/about/contact")
 def contact() -> ResponseValue:
     return render_template("about/contact.html")
 
 
+#### Redirects
+
+
 @base.route("/about/covid")
 def covid() -> ResponseValue:
     return redirect(url_for(".page", page_name="health"), code=301)
+
+
+@base.route("/about/diversity")
+def about_diversity() -> ResponseValue:
+    return redirect(url_for(".org_page", page_name="diversity"))
+
+
+@base.route("/about/diversity/<int:year>")
+def yearly_diversity_stats_redirect(year: int) -> ResponseValue:
+    return redirect(url_for(".yearly_diversity_stats", year=year))
+
+
+@base.route("/company")
+def company_redirect() -> ResponseValue:
+    return redirect(url_for("base.company"))
--- apps/base/organisation.py
+++ apps/base/organisation.py
@@ -3,11 +3,47 @@ Pages under /organisation - this namespace is for information about EMF the orga
 rather than EMF the event.
 """
 
+import json
+
 from flask import render_template
+from flask.typing import ResponseValue
+
+from apps.common import render_markdown
 
 from . import base
+
+
+@base.route("/organisation")
+def organisation() -> ResponseValue:
+    return render_template("organisation/index.html")
 
 
 @base.route("/organisation/finances")
 def finances():
     return render_template("organisation/finances.html")
+
+
+@base.route("/organisation/company")
+def company() -> ResponseValue:
+    return render_markdown("organisation/company", page_name="company", template="organisation/template.html")
+
+
+@base.route("/organisation/<page_name>")
+def org_page(page_name: str) -> ResponseValue:
+    return render_markdown(
+        f"organisation/{page_name}", page_name=page_name, template="organisation/template.html"
+    )
+
+
+@base.route("/organisation/diversity/<int:year>")
+def yearly_diversity_stats(year: int) -> ResponseValue:
+    if year in (2018, 2022):
+        with open(f"exports/{year}/public/UserDiversity.json") as raw_data:
+            data = json.load(raw_data)
+
+        return render_template(
+            "about/diversity/pre-2024-stats.html",
+            year=year,
+            data=data["diversity"],
+        )
+    return render_markdown(f"about/diversity/{year}")
