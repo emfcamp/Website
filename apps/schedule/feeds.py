e3f58bb8e95d10707c90e1e02ba54aa24d97b6c0
Franziska Kunsmann <hi@kunsmann.eu>
2026-03-14 14:51:39 +0100

    apps/schedule/feeds: fix missing url_for()

--- apps/schedule/feeds.py
+++ apps/schedule/feeds.py
@@ -59,7 +59,7 @@ def schedule_frab(year):
     if year != event_year():
         return feed_historic(year, "frab")
 
-    return redirect("schedule.schedule_frab_xml", year=year)
+    return redirect(url_for("schedule.schedule_frab_xml", year=year))
 
 
 @schedule.route("/schedule/<int:year>.frab.xml")
