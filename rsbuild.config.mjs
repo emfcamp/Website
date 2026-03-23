5ac1fc4cf1d4e683410b41a0545e386857918cb4
Jon Wood <jon@blankpad.net>
2026-03-23 15:36:17 +0000

    Fix volunteer schedule Javascript build

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,23 +1,31 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginSass } from '@rsbuild/plugin-sass';
+import { defineConfig } from "@rsbuild/core";
+import { pluginSass } from "@rsbuild/plugin-sass";
 
 class SCSSNoOutputPlugin {
     apply(compiler) {
-        compiler.hooks.thisCompilation.tap('SCSSNoOutputPlugin', compilation => {
-            const { Compilation } = compiler.rspack;
-            compilation.hooks.processAssets.tap({
-                name: 'SCSSNoOutputPlugin',
-                stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
-            }, () => {
-                for (const asset of compilation.getAssets()) {
-                    // If this looks like a JS file corresponding to a SCSS file...
-                    if (asset.name.match(/^static\/js\/.*\.scss\.js$/)) {
-                        // then don't delete it - it'll be empty anyway.
-                        compilation.deleteAsset(asset.name);
-                    }
-                }
-            });
-        });
+        compiler.hooks.thisCompilation.tap(
+            "SCSSNoOutputPlugin",
+            (compilation) => {
+                const { Compilation } = compiler.rspack;
+                compilation.hooks.processAssets.tap(
+                    {
+                        name: "SCSSNoOutputPlugin",
+                        stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
+                    },
+                    () => {
+                        for (const asset of compilation.getAssets()) {
+                            // If this looks like a JS file corresponding to a SCSS file...
+                            if (
+                                asset.name.match(/^static\/js\/.*\.scss\.js$/)
+                            ) {
+                                // then don't delete it - it'll be empty anyway.
+                                compilation.deleteAsset(asset.name);
+                            }
+                        }
+                    },
+                );
+            },
+        );
     }
 }
 
@@ -26,7 +34,7 @@ export default defineConfig({
     performance: {
         chunkSplit: {
             // Other strategies require Flask to read the static files manifest to include all necessary JS chunks.
-            strategy: 'all-in-one',
+            strategy: "all-in-one",
         },
     },
     dev: {
@@ -46,83 +54,80 @@ export default defineConfig({
             config.module.rules.push({
                 test: /\.jsx$/,
                 use: {
-                    loader: 'builtin:swc-loader',
+                    loader: "builtin:swc-loader",
                     options: {
                         jsc: {
                             parser: {
-                                syntax: 'ecmascript',
+                                syntax: "ecmascript",
                                 jsx: true,
                             },
                         },
                     },
                 },
-                type: 'javascript/auto',
+                type: "javascript/auto",
             });
             return config;
         },
     },
     source: {
         entry: {
-            "main.js": './js/main.js',
-            "line_up.js": './js/line-up.js',
-            "schedule.js": './js/schedule.js',
-            "volunteer_schedule.js": './js/volunteer-schedule.js',
-            "lottery.js": './js/lottery.js',
-            "arrivals.js": './js/arrivals.js',
+            "main.js": "./js/main.js",
+            "line_up.js": "./js/line-up.js",
+            "schedule.js": "./js/schedule.js",
+            "volunteer_schedule.js": "./js/volunteer_schedule.js",
+            "lottery.js": "./js/lottery.js",
+            "arrivals.js": "./js/arrivals.js",
 
-            "admin.scss": './css/admin.scss',
-            "arrivals.scss": './css/arrivals.scss',
-            "invoice.scss": './css/invoice.scss',
-            "main.scss": './css/main.scss',
-            "receipt.scss": './css/receipt.scss',
-            "schedule.scss": './css/schedule.scss',
-            "volunteer_schedule.scss": './css/volunteer_schedule.scss',
-            "flask-admin.scss": './css/flask-admin.scss',
+            "admin.scss": "./css/admin.scss",
+            "arrivals.scss": "./css/arrivals.scss",
+            "invoice.scss": "./css/invoice.scss",
+            "main.scss": "./css/main.scss",
+            "receipt.scss": "./css/receipt.scss",
+            "schedule.scss": "./css/schedule.scss",
+            "volunteer_schedule.scss": "./css/volunteer_schedule.scss",
+            "flask-admin.scss": "./css/flask-admin.scss",
         },
     },
     output: {
         filenameHash: false, // flask will do this for us
-        manifest: './staticmanifest.json',
+        manifest: "./staticmanifest.json",
         copy: [
-            { from: './manifest.json', to: 'static' },
-            { from: './images', to: 'static/images' },
+            { from: "./manifest.json", to: "static" },
+            { from: "./images", to: "static/images" },
             {
-                context: './node_modules/@primer/octicons/build/svg',
-                from: '*.svg',
-                to: 'static/icons'
+                context: "./node_modules/@primer/octicons/build/svg",
+                from: "*.svg",
+                to: "static/icons",
             },
             {
-                from: './static/**/*',
-                to: '',
+                from: "./static/**/*",
+                to: "",
                 globOptions: {
                     // Don't copy things we're going to output ourselves.
-                    ignore: [
-                        '**/fonts/**',
-                        '**/flask-admin.css',
-                    ],
+                    ignore: ["**/fonts/**", "**/flask-admin.css"],
                 },
             },
         ],
         filename: {
             js: (pathData) => {
                 // Don't use .js.js filenames.
-                if (pathData.chunk.name.endsWith('.js')) {
+                if (pathData.chunk.name.endsWith(".js")) {
                     return pathData.chunk.name;
                 }
-                return '[name].js';
+                return "[name].js";
             },
             css: (pathData) => {
                 // Don't use .scss.css filenames.
-                if (pathData.chunk.name.endsWith('.scss')) {
-                    return pathData.chunk.name.replace('.scss', '.css');
+                if (pathData.chunk.name.endsWith(".scss")) {
+                    return pathData.chunk.name.replace(".scss", ".css");
                 }
-                return '[name].css';
-            }
+                return "[name].css";
+            },
         },
         distPath: {
-            html: '', // don't output HTML
-            font: 'static/fonts', // use font*s* directory, rather than 'font'
-            image: 'static/images', // use image*s* directory, rather than 'image'
+            html: "", // don't output HTML
+            font: "static/fonts", // use font*s* directory, rather than 'font'
+            image: "static/images", // use image*s* directory, rather than 'image'
         },
-    }
+    },
 });
