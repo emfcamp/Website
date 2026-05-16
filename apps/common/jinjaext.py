d375dfe02bad3f8ec186a43373ba05b613bd188f
Luke Granger-Brown <git@lukegb.com>
2026-05-16 19:33:27 +0100

    Make include_markdown its own tag, and do Jinja-then-Markdown

--- apps/common/jinjaext.py
+++ apps/common/jinjaext.py
@@ -1,15 +1,32 @@
 """Jinja2 extensions."""
 
+from typing import ClassVar
+
 from jinja2.ext import Extension
-from markdown import markdown
+from jinja2.nodes import Const, ContextReference, Output
+from jinja2.parser import Parser
+from jinja2.runtime import Context
+from markupsafe import Markup
+
+from . import render_trusted_markdown
 
 
 class IncludemdExtension(Extension):
-    def preprocess(self, source, name, filename=None):
-        is_markdown = (filename and filename.endswith(".md")) or (name and name.endswith(".md"))
-        if not is_markdown:
-            return source
-        return markdown(
-            source,
-            extensions=["markdown.extensions.nl2br"],
-        )
+    """Adds a `include_markdown` tag to Jinja that works like 'include' but for
+    Markdown."""
+
+    tags: ClassVar[set[str]] = {"include_markdown"}  # type: ignore[misc]
+
+    def parse(self, parser: Parser) -> Output:
+        lineno = next(parser.stream).lineno
+        template_name = parser.parse_expression()
+        parent_template_name = Const(parser.name)
+        args = [template_name, parent_template_name, ContextReference()]
+        call = self.call_method("_render_markdown", args, lineno=lineno)
+        return Output([call], lineno=lineno)
+
+    def _render_markdown(self, template_name: str, parent_template_name: str, context: Context) -> Markup:
+        template = self.environment.get_template(template_name, parent_template_name)
+        gen = template.root_render_func(template.new_context(context.get_all(), True))
+        value = self.environment.concat(gen)
+        return render_trusted_markdown(value)
