f8a05df6a6d2175bf7b1dc52b29a59882aeaedf0
Jon Wood <jon@blankpad.net>
2026-08-09 13:01:16 +0100

    Update recap in the face of real data

--- apps/volunteer/recap.py
+++ apps/volunteer/recap.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 from flask import flash, redirect, render_template, url_for
 from flask.typing import ResponseReturnValue
 from flask_login import current_user
@@ -10,17 +8,10 @@ from . import v_admin_required, v_user_required, volunteer
 
 
 def _recap_for(volunteer: Volunteer) -> ResponseReturnValue:
-    total_shifts = reduce(
-        lambda x, y: x + y, [entry.voucher_contribution for entry in current_user.shift_entries]
-    )
-
     return render_template(
         "volunteer/recap.html",
         volunteer=volunteer,
-        shift_entries=current_user.shift_entries,
-        registered_for_build=volunteer.registered_for_buildup,
-        total_shifts=total_shifts,
-        qualifies_for_voucher=(total_shifts >= 2),
+        shift_entries=volunteer.user.shift_entries,
     )
 
 
